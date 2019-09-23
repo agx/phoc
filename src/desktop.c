aec0a1abdf57a1e3db77e89cb32593f54f150c7f
Guido Günther <agx@sigxcpu.org>
2019-09-23 04:21:13 -0700

    Use old gamma control protocol if available
    This is based on version number but should we decide to keep
    support for this protocol in our embedded wlroot for longer
    we can add that as well.

--- src/desktop.c
+++ src/desktop.c
@@ -14,7 +14,9 @@
 #include <wlr/types/wlr_data_control_v1.h>
 #include <wlr/types/wlr_export_dmabuf_v1.h>
 #include <wlr/types/wlr_gamma_control_v1.h>
-#include <wlr/types/wlr_gamma_control.h>
+#ifdef PHOC_USE_GAMMA_CONTROL
+#  include <wlr/types/wlr_gamma_control.h>
+#endif
 #include <wlr/types/wlr_gtk_primary_selection.h>
 #include <wlr/types/wlr_idle_inhibit_v1.h>
 #include <wlr/types/wlr_idle.h>
@@ -469,7 +471,9 @@ phoc_desktop_constructed (GObject *object)
   }
 #endif
 
+#ifdef PHOC_USE_GAMMA_CONTROL
   self->gamma_control_manager = wlr_gamma_control_manager_create(server->wl_display);
+#endif
   self->gamma_control_manager_v1 = wlr_gamma_control_manager_v1_create(server->wl_display);
   self->screenshooter = wlr_screenshooter_create(server->wl_display);
   self->export_dmabuf_manager_v1 =
