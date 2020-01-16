ecd4c96297c83357a4577673cf47a718eec85a08
Guido Günther <agx@sigxcpu.org>
2020-01-16 19:40:30 +0100

    Add initial test
    Tnis just creates a server object.

---
+++ tests/test-server.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020 Purism SPC
+ * SPDX-License-Identifier: GPL-3.0+
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#include "server.h"
+
+static void
+test_phoc_server_get_default (void)
+{
+  PhocServer *server = phoc_server_get_default ();
+  PhocServer *server2;
+
+  g_assert_true (PHOC_IS_SERVER (server));
+
+  server2 = phoc_server_get_default ();
+  g_assert_true (server2 == server);
+}
+
+static void
+test_phoc_server_setup (void)
+{
+  PhocServer *server = phoc_server_get_default ();
+
+  g_assert_true (PHOC_IS_SERVER (server));
+
+  g_assert_true (phoc_server_setup(server, 0, NULL));
+}
+
+gint
+main (gint argc, gchar *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func("/phoc/server/get_default", test_phoc_server_get_default);
+  g_test_add_func("/phoc/server/setup", test_phoc_server_setup);
+
+  return g_test_run();
+}
