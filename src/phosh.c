39c0729ab5a8cd93ab590d8ae633ebfc02745e12
Guido Günther <agx@sigxcpu.org>
2020-06-08 10:15:35 +0200

    phosh: Drop xdg-switcher interface
    Instead of requiring a phosh/phoc lockstep upgrade post an error if a
    client wants to bind the protcol. This keeps the code we need carry
    minimal but we can still be backward compatible (this is not breaking
    clients that don't use that part of the protocol).

--- src/phosh.c
+++ src/phosh.c
@@ -23,196 +23,6 @@
 
 #define PHOSH_PRIVATE_VERSION 3
 
-static void
-xdg_switcher_handle_list_xdg_surfaces(struct wl_client *client,
-				      struct wl_resource *resource)
-{
-  struct phosh_private_xdg_switcher *xdg_switcher =
-    phosh_private_xdg_switcher_from_resource(resource);
-  struct phosh_private *phosh = xdg_switcher->phosh;
-  PhocDesktop *desktop = phosh->desktop;
-  struct roots_view *view;
-
-  wl_list_for_each(view, &desktop->views, link) {
-    const char *app_id = NULL;
-    const char *title = NULL;
-
-    switch (view->type) {
-    case ROOTS_XDG_SHELL_VIEW: {
-      struct roots_xdg_surface *xdg_surface =
-	roots_xdg_surface_from_view(view);
-      if (xdg_surface->xdg_surface->role != WLR_XDG_SURFACE_ROLE_TOPLEVEL)
-	continue;
-      app_id = xdg_surface->xdg_surface->toplevel->app_id;
-      title = xdg_surface->xdg_surface->toplevel->title;
-      break;
-    }
-    case ROOTS_XDG_SHELL_V6_VIEW: {
-      struct roots_xdg_surface_v6 *xdg_surface_v6 =
-	roots_xdg_surface_v6_from_view(view);
-
-      if (xdg_surface_v6->xdg_surface_v6->role != WLR_XDG_SURFACE_V6_ROLE_TOPLEVEL)
-	continue;
-      app_id = xdg_surface_v6->xdg_surface_v6->toplevel->app_id;
-      title = xdg_surface_v6->xdg_surface_v6->toplevel->title;
-      break;
-    }
-    default:
-      /* other surface types would go here */
-      break;
-    }
-
-    if (app_id) {
-      phosh_private_xdg_switcher_send_xdg_surface (resource, app_id, title);
-      app_id = NULL;
-      title = NULL;
-    }
-  }
-  phosh_private_xdg_switcher_send_list_xdg_surfaces_done (resource);
-}
-
-
-static void
-xdg_switcher_handle_raise_xdg_surfaces(struct wl_client *client,
-				       struct wl_resource *resource,
-				       const char *app_id,
-				       const char *title)
-{
-  struct phosh_private_xdg_switcher *xdg_switcher =
-    phosh_private_xdg_switcher_from_resource(resource);
-  struct phosh_private *phosh = xdg_switcher->phosh;
-  PhocDesktop *desktop = phosh->desktop;
-  PhocServer *server = phoc_server_get_default ();
-  struct roots_view *view, *found_view = NULL;
-  struct roots_input *input = server->input;
-  struct roots_seat *seat = input_last_active_seat(input);
-
-  g_debug ("will raise view %s", app_id);
-  wl_list_for_each(view, &desktop->views, link) {
-    switch (view->type) {
-    case ROOTS_XDG_SHELL_VIEW: {
-      struct roots_xdg_surface *xdg_surface =
-	roots_xdg_surface_from_view(view);
-
-      if (xdg_surface->xdg_surface->role != WLR_XDG_SURFACE_ROLE_TOPLEVEL)
-	continue;
-      if (!strcmp(app_id, xdg_surface->xdg_surface->toplevel->app_id) &&
-	  !strcmp(title, xdg_surface->xdg_surface->toplevel->title))
-	found_view = view;
-      break;
-    }
-    case ROOTS_XDG_SHELL_V6_VIEW: {
-      struct roots_xdg_surface_v6 *xdg_surface_v6 =
-	roots_xdg_surface_v6_from_view(view);
-
-      if (xdg_surface_v6->xdg_surface_v6->role != WLR_XDG_SURFACE_V6_ROLE_TOPLEVEL)
-	continue;
-      if (!strcmp(app_id, xdg_surface_v6->xdg_surface_v6->toplevel->app_id) &&
-	  !strcmp(title, xdg_surface_v6->xdg_surface_v6->toplevel->title))
-	found_view = view;
-      break;
-    }
-    default:
-      /* other surface types would go here */
-      break;
-    }
-  }
-
-  /* TODO: check if view belongs to this seat */
-  if (found_view) {
-    roots_seat_set_focus(seat, found_view);
-  }
-}
-
-static void xdg_switcher_handle_close_xdg_surfaces(struct wl_client *client,
-						   struct wl_resource *resource,
-						   const char *app_id,
-						   const char *title) {
-  struct phosh_private_xdg_switcher *xdg_switcher =
-    phosh_private_xdg_switcher_from_resource(resource);
-  struct phosh_private *phosh = xdg_switcher->phosh;
-  PhocDesktop *desktop = phosh->desktop;
-  struct roots_view *view, *found_view;
-
-  g_debug ("Will close view %s: %s", app_id, title ? :"");
-  wl_list_for_each(view, &desktop->views, link) {
-    found_view = NULL;
-
-    switch (view->type) {
-    case ROOTS_XDG_SHELL_VIEW: {
-      struct roots_xdg_surface *xdg_surface =
-	roots_xdg_surface_from_view(view);
-      if (xdg_surface->xdg_surface->role != WLR_XDG_SURFACE_ROLE_TOPLEVEL)
-	continue;
-
-      if (!g_strcmp0 (app_id, xdg_surface->xdg_surface->toplevel->app_id)) {
-	if (title) {
-	  if (!g_strcmp0 (title, xdg_surface->xdg_surface->toplevel->title))
-	    found_view = view;
-	} else {
-	  found_view = view;
-	}
-      }
-      break;
-    }
-    case ROOTS_XDG_SHELL_V6_VIEW: {
-      struct roots_xdg_surface_v6 *xdg_surface_v6 =
-	roots_xdg_surface_v6_from_view(view);
-
-      if (xdg_surface_v6->xdg_surface_v6->role != WLR_XDG_SURFACE_V6_ROLE_TOPLEVEL)
-	continue;
-      if (!strcmp(app_id, xdg_surface_v6->xdg_surface_v6->toplevel->app_id)) {
-	if (title) {
-	  if (!strcmp(title, xdg_surface_v6->xdg_surface_v6->toplevel->title))
-	    found_view = view;
-	} else {
-	  found_view = view;
-	}
-      }
-      break;
-    }
-    default:
-      /* other surface types would go here */
-      break;
-    }
-
-    if (found_view) {
-      view_close(found_view);
-      /* we might have toplevels with same app_id and
-	 title so close all of them */
-    }
-  }
-}
-
-static void
-xdg_switcher_handle_destroy(struct wl_client *client,
-			    struct wl_resource *resource)
-{
-  wl_resource_destroy(resource);
-}
-
-
-static void
-xdg_switcher_handle_resource_destroy(struct wl_resource *resource)
-{
-  struct phosh_private_xdg_switcher *xdg_switcher =
-    phosh_private_xdg_switcher_from_resource(resource);
-
-  g_debug ("Destroying xdg_switcher %p (res %p)", xdg_switcher,
-	   xdg_switcher->resource);
-  wl_list_remove(&xdg_switcher->link);
-  free(xdg_switcher);
-}
-
-
-static const struct phosh_private_xdg_switcher_interface phosh_private_xdg_switcher_impl = {
-  .destroy = xdg_switcher_handle_destroy,
-  .list_xdg_surfaces = xdg_switcher_handle_list_xdg_surfaces,
-  .raise_xdg_surface = xdg_switcher_handle_raise_xdg_surfaces,
-  .close_xdg_surface = xdg_switcher_handle_close_xdg_surfaces,
-};
-
-
 static
 void phosh_rotate_display(struct wl_client *client,
 			  struct wl_resource *resource,
@@ -288,35 +98,13 @@ handle_get_xdg_switcher(struct wl_client *client,
 			struct wl_resource *phosh_private_resource,
 			uint32_t id)
 {
-  struct phosh_private *phosh =
-    phosh_private_from_resource(phosh_private_resource);
-
-  struct phosh_private_xdg_switcher *xdg_switcher =
-    calloc(1, sizeof(struct phosh_private_xdg_switcher));
-  if (xdg_switcher == NULL) {
-    wl_client_post_no_memory(client);
-    return;
-  }
-
   int version = wl_resource_get_version(phosh_private_resource);
-  xdg_switcher->resource = wl_resource_create(client,
-					      &phosh_private_xdg_switcher_interface, version, id);
-  if (xdg_switcher->resource == NULL) {
-    free(xdg_switcher);
-    wl_client_post_no_memory(client);
-    return;
-  }
+  struct wl_resource *resource  = wl_resource_create(client, &phosh_private_xdg_switcher_interface,
+						     version, id);
 
-  g_debug ("new phosh_private_xdg_switcher %p (res %p)", xdg_switcher,
-	   xdg_switcher->resource);
-  wl_resource_set_implementation(xdg_switcher->resource,
-				 &phosh_private_xdg_switcher_impl, xdg_switcher, xdg_switcher_handle_resource_destroy);
-
-  xdg_switcher->phosh = phosh;
-  wl_signal_init(&xdg_switcher->events.destroy);
-  wl_list_insert(&phosh->xdg_switchers, &xdg_switcher->link);
+  wl_resource_post_error(resource, WL_DISPLAY_ERROR_INVALID_OBJECT,
+			 "Use wlr-toplevel-management protocol instead");
 }
-
 
 
 static void
@@ -405,10 +193,3 @@ struct phosh_private
   return wl_resource_get_user_data(resource);
 }
 
-
-struct phosh_private_xdg_switcher *
-phosh_private_xdg_switcher_from_resource(struct wl_resource *resource) {
-  assert(wl_resource_instance_of(resource, &phosh_private_xdg_switcher_interface,
-				 &phosh_private_xdg_switcher_impl));
-  return wl_resource_get_user_data(resource);
-}
