f3952d4a583ba173a0645ce89eecc8765d94743e
Clayton Craft <clayton@craftyguy.net>
2020-10-18 15:58:11 -0700

    utils: apply phosh code formatting styles
    This is a cosmetic change that applies code formatting styles from
    Phosh's style guidelines to utils.c/h.

--- src/utils.c
+++ src/utils.c
@@ -3,7 +3,8 @@
 #include <wlr/version.h>
 #include "utils.h"
 
-void phoc_utils_fix_transform(enum wl_output_transform *transform)
+void
+phoc_utils_fix_transform (enum wl_output_transform *transform)
 {
   /*
    * Starting from version 0.11.0, wlroots rotates counter-clockwise, while
@@ -12,22 +13,22 @@ void phoc_utils_fix_transform(enum wl_output_transform *transform)
    * before applying it
    */
 #if (WLR_VERSION_MAJOR > 0 || WLR_VERSION_MINOR > 10)
-  switch(*transform) {
-    case WL_OUTPUT_TRANSFORM_90:
-      *transform = WL_OUTPUT_TRANSFORM_270;
-      break;
-    case WL_OUTPUT_TRANSFORM_270:
-      *transform = WL_OUTPUT_TRANSFORM_90;
-      break;
-    case WL_OUTPUT_TRANSFORM_FLIPPED_90:
-      *transform = WL_OUTPUT_TRANSFORM_FLIPPED_270;
-      break;
-    case WL_OUTPUT_TRANSFORM_FLIPPED_270:
-      *transform = WL_OUTPUT_TRANSFORM_FLIPPED_90;
-      break;
-    default:
-      /* Nothing to be done */
-      break;
+  switch (*transform) {
+  case WL_OUTPUT_TRANSFORM_90:
+    *transform = WL_OUTPUT_TRANSFORM_270;
+    break;
+  case WL_OUTPUT_TRANSFORM_270:
+    *transform = WL_OUTPUT_TRANSFORM_90;
+    break;
+  case WL_OUTPUT_TRANSFORM_FLIPPED_90:
+    *transform = WL_OUTPUT_TRANSFORM_FLIPPED_270;
+    break;
+  case WL_OUTPUT_TRANSFORM_FLIPPED_270:
+    *transform = WL_OUTPUT_TRANSFORM_FLIPPED_90;
+    break;
+  default:
+    /* Nothing to be done */
+    break;
   }
 #endif
 }
--- src/utils.h
+++ src/utils.h
@@ -2,4 +2,4 @@
 
 #include <wlr/types/wlr_output_layout.h>
 
-void phoc_utils_fix_transform(enum wl_output_transform *transform);
+void phoc_utils_fix_transform (enum wl_output_transform *transform);
