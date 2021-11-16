agx/phoc

commit cc0e4e108cd62eeaef91f5e58a0e0e89420cf60a

touch: Use input-device as base class

src/touch.h
@@ -26,9 +26,6 @@ struct _PhocTouch {
 
   struct wl_list           link; // seat::touch
   struct wl_listener       touch_destroy;
-  PhocSeat                *seat;
-  struct wlr_input_device *device;
-
 };
 
 PhocTouch *phoc_touch_new (struct wlr_input_device *device, PhocSeat *seat);

src/seat.c
@@ -629,7 +629,8 @@ phoc_seat_configure_cursor (PhocSeat *seat)
     seat_reset_device_mappings (seat, device);
   }
   wl_list_for_each (touch, &seat->touch, link) {
-    seat_reset_device_mappings (seat, touch->device);
+    struct wlr_input_device *device = phoc_input_device_get_device (PHOC_INPUT_DEVICE (touch));
+    seat_reset_device_mappings (seat, device);
   }
   wl_list_for_each (tablet, &seat->tablets, link) {
     seat_reset_device_mappings (seat, tablet->device);
@@ -646,13 +647,13 @@ phoc_seat_configure_cursor (PhocSeat *seat)
                                        output);
     }
     wl_list_for_each (touch, &seat->touch, link) {
-      seat_set_device_output_mappings (seat, touch->device,
-                                       output);
+      struct wlr_input_device *device = phoc_input_device_get_device (PHOC_INPUT_DEVICE (touch));
+      seat_set_device_output_mappings (seat, device, output);
       g_debug ("Added mapping for touch device '%s' to output '%s'",
-               touch->device->name,
+               device->name,
                output->wlr_output->name);
       g_hash_table_insert (desktop->input_output_map,
-                           g_strdup (touch->device->name),
+                           g_strdup (device->name),
                            output);
     }
   }
@@ -1063,14 +1064,15 @@ seat_add_switch (PhocSeat                *seat,
 static void
 handle_touch_destroy (PhocTouch *touch)
 {
-  PhocSeat *seat = touch->seat;
+  PhocSeat *seat = phoc_input_device_get_seat (PHOC_INPUT_DEVICE (touch));
   PhocServer *server = phoc_server_get_default ();
   PhocDesktop *desktop = server->desktop;
+  struct wlr_input_device *device = phoc_input_device_get_device (PHOC_INPUT_DEVICE (touch));
 
-  g_debug ("Removing touch device: %s", touch->device->name);
-  g_hash_table_remove (desktop->input_output_map, touch->device->name);
+  g_debug ("Removing touch device: %s", device->name);
+  g_hash_table_remove (desktop->input_output_map, device->name);
   wl_list_remove (&touch->link);
-  wlr_cursor_detach_input_device (seat->cursor->cursor, touch->device);
+  wlr_cursor_detach_input_device (seat->cursor->cursor, device);
   g_object_unref (touch);
 
   seat_update_capabilities (seat);

src/touch.c
@@ -7,18 +7,11 @@
 #include <time.h>
 #include <wlr/backend/libinput.h>
 
+#include "input-device.h"
 #include "touch.h"
 #include "seat.h"
 
-G_DEFINE_TYPE (PhocTouch, phoc_touch, G_TYPE_OBJECT);
-
-enum {
-  PROP_0,
-  PROP_SEAT,
-  PROP_DEVICE,
-  PROP_LAST_PROP
-};
-static GParamSpec *props[PROP_LAST_PROP];
+G_DEFINE_TYPE (PhocTouch, phoc_touch, PHOC_TYPE_INPUT_DEVICE);
 
 enum {
   TOUCH_DESTROY,
@@ -39,9 +32,10 @@ static void
 phoc_touch_constructed (GObject *object)
 {
   PhocTouch *self = PHOC_TOUCH (object);
+  struct wlr_input_device *device = phoc_input_device_get_device (PHOC_INPUT_DEVICE (self));
 
   self->touch_destroy.notify = handle_touch_destroy;
-  wl_signal_add (&self->device->events.destroy, &self->touch_destroy);
+  wl_signal_add (&device->events.destroy, &self->touch_destroy);
 
   G_OBJECT_CLASS (phoc_touch_parent_class)->constructed (object);
 }
@@ -61,31 +55,13 @@ phoc_touch_class_init (PhocTouchClass *klass)
 {
   GObjectClass *object_class = (GObjectClass *)klass;
 
-  object_class->set_property = phoc_touch_set_property;
-  object_class->get_property = phoc_touch_get_property;
-
   object_class->constructed = phoc_touch_constructed;
   object_class->finalize = phoc_touch_finalize;
-
-  props[PROP_DEVICE] =
-    g_param_spec_pointer (
-      "device",
-      "Device",
-      "The device object",
-      G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS | G_PARAM_EXPLICIT_NOTIFY);
-  props[PROP_SEAT] =
-    g_param_spec_pointer (
-      "seat",
-      "Seat",
-      "The seat object",
-      G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS | G_PARAM_EXPLICIT_NOTIFY);
-  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 
   signals[TOUCH_DESTROY] = g_signal_new ("touch-destroyed",
                                          G_TYPE_FROM_CLASS (klass),
                                          G_SIGNAL_RUN_LAST,
                                          0, NULL, NULL, NULL, G_TYPE_NONE, 0);
-
 }
 
 

commit c2014ede6d8e09c4165d4c5fe7a1c8f442fecb1c

touch: Use correct parent class
Fixes: cc0e4e10 ("touch: Use input-device as base class")

src/touch.h
@@ -5,17 +5,18 @@
 #include <wlr/types/wlr_input_device.h>
 #include <wlr/types/wlr_seat.h>
 #include "settings.h"
+#include "input-device.h"
 
 G_BEGIN_DECLS
 
 #define PHOC_TYPE_TOUCH (phoc_touch_get_type ())
 
-G_DECLARE_FINAL_TYPE (PhocTouch, phoc_touch, PHOC, TOUCH, GObject);
+G_DECLARE_FINAL_TYPE (PhocTouch, phoc_touch, PHOC, TOUCH, PhocInputDevice);
 
 /* TODO: we keep the struct public due to the list links and
    notifiers but we should avoid other member access */
 struct _PhocTouch {
-  GObject                  parent;
+  PhocInputDevice          parent;
 
   struct wl_list           link; // seat::touch
   struct wl_listener       touch_destroy;