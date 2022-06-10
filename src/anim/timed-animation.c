12f38b296b1f86ca30579d0bf78ce3e5c84efd3a
Guido Günther <agx@sigxcpu.org>
2022-06-10 13:26:09 +0200

    timed-animation: Add transfer annotations
    Helps gi-docgen but also makes things more obvious.
    
    Fixes 9310a8da ("Add timed animations")

--- src/anim/timed-animation.c
+++ src/anim/timed-animation.c
@@ -333,6 +333,12 @@ phoc_timed_animation_new (void)
 }
 
 
+/**
+ * phoc_timed_animation_get_property_easer:
+ * @self: a `PhocTimedAnimation`
+ *
+ * Returns: (transfer none): The property easer of this time animation.
+ */
 PhocPropertyEaser *
 phoc_timed_animation_get_property_easer (PhocTimedAnimation *self)
 {
@@ -365,6 +371,12 @@ phoc_timed_animation_get_duration (PhocTimedAnimation *self)
 }
 
 
+/**
+ * phoc_timed_animation_get_animatable:
+ * @self: a `PhocTimedAnimation`
+ *
+ * Returns: (transfer none): The animatable of this timed animation.
+ */
 PhocAnimatable *
 phoc_timed_animation_get_animatable (PhocTimedAnimation *self)
 {
