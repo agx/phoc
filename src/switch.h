f240b5538a3efdb9fddc43888ea6cdc4f6e1672d
Guido Günther <agx@sigxcpu.org>
2022-06-10 14:59:18 +0200

    switch: Use phoc_ prefix consistently

--- src/switch.h
+++ src/switch.h
@@ -4,16 +4,16 @@
 
 G_BEGIN_DECLS
 
-struct roots_switch {
+typedef struct phoc_switch {
 	PhocSeat *seat;
 	struct wlr_input_device *device;
 	struct wl_listener device_destroy;
 
 	struct wl_listener toggle;
 	struct wl_list link;
-};
+} PhocSwitch;
 
-void roots_switch_handle_toggle(struct roots_switch *switch_device,
+void phoc_switch_handle_toggle(struct phoc_switch *switch_device,
 		struct wlr_event_switch_toggle *event);
 
 G_END_DECLS
