agx/phoc

commit 6a508d5467395edba6ad378d25fbe8412771c826

Adjust to wlroots 0.15

src/xdg_shell.c
@@ -7,9 +7,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <wayland-server-core.h>
-#include <wlr/types/wlr_box.h>
 #include <wlr/types/wlr_surface.h>
 #include <wlr/types/wlr_xdg_shell.h>
+#include <wlr/util/box.h>
 #include "cursor.h"
 #include "desktop.h"
 #include "input.h"
@@ -202,7 +202,7 @@ static void handle_request_maximize(struct wl_listener *listener, void *data) {
 		return;
 	}
 
-	if (surface->toplevel->client_pending.maximized) {
+	if (surface->toplevel->pending.maximized) {
 		view_maximize(view, NULL);
 	} else {
 		view_restore(view);
@@ -270,7 +270,7 @@ static void handle_surface_commit(struct wl_listener *listener, void *data) {
 
 	uint32_t pending_serial =
 		roots_surface->pending_move_resize_configure_serial;
-	if (pending_serial > 0 && pending_serial >= surface->configure_serial) {
+	if (pending_serial > 0 && pending_serial >= surface->current.configure_serial) {
 		double x = view->box.x;
 		double y = view->box.y;
 		if (view->pending_move_resize.update_x) {
@@ -291,7 +291,7 @@ static void handle_surface_commit(struct wl_listener *listener, void *data) {
 		}
 		view_update_position(view, x, y);
 
-		if (pending_serial == surface->configure_serial) {
+		if (pending_serial == surface->current.configure_serial) {
 			roots_surface->pending_move_resize_configure_serial = 0;
 		}
 	}
@@ -365,11 +365,12 @@ void handle_xdg_shell_surface(struct wl_listener *listener, void *data) {
 		PhocXdgSurface *parent = surface->toplevel->parent->data;
 		view_set_parent(&phoc_surface->view, &parent->view);
 	}
-	if (surface->toplevel->client_pending.maximized) {
+
+	if (surface->toplevel->pending.maximized) {
 		view_maximize(&phoc_surface->view, NULL);
 	}
-	phoc_view_set_fullscreen(&phoc_surface->view, surface->toplevel->client_pending.fullscreen,
-		surface->toplevel->client_pending.fullscreen_output);
+	phoc_view_set_fullscreen(&phoc_surface->view, surface->toplevel->pending.fullscreen,
+		surface->toplevel->requested.fullscreen_output);
 	view_auto_maximize(&phoc_surface->view);
 	view_set_title(&phoc_surface->view, surface->toplevel->title);
 
@@ -433,7 +434,7 @@ static void decoration_handle_request_mode(struct wl_listener *listener,
 		wl_container_of(listener, decoration, request_mode);
 
 	enum wlr_xdg_toplevel_decoration_v1_mode mode =
-		decoration->wlr_decoration->client_pending_mode;
+		decoration->wlr_decoration->requested_mode;
 	if (mode == WLR_XDG_TOPLEVEL_DECORATION_V1_MODE_NONE) {
 		mode = WLR_XDG_TOPLEVEL_DECORATION_V1_MODE_CLIENT_SIDE;
 	}
@@ -445,7 +446,7 @@ static void decoration_handle_surface_commit(struct wl_listener *listener,
 	struct roots_xdg_toplevel_decoration *decoration =
 		wl_container_of(listener, decoration, surface_commit);
 
-	bool decorated = decoration->wlr_decoration->current_mode ==
+	bool decorated = decoration->wlr_decoration->current.mode ==
 		WLR_XDG_TOPLEVEL_DECORATION_V1_MODE_SERVER_SIDE;
 	view_update_decorated(&decoration->surface->view, decorated);
 }

meson.build
@@ -66,7 +66,7 @@ if not embed_wlroots.disabled() and wlroots_proj.found()
         have_wlr_remove_startup_info = true
         have_wlr_xdg_activation_v1_add_token = true
 else
-        wlroots = dependency('wlroots', version: '>= 0.14.1')
+        wlroots = dependency('wlroots', version: '== 0.15.0')
         wlroots_has_xwayland = cc.get_define('WLR_HAS_XWAYLAND', prefix: '#include <wlr/config.h>', dependencies: wlroots) == '1'
         wlroots_has_output_power_management = cc.has_header('wlr/types/wlr_output_power_management_v1.h', dependencies: wlroots)
         have_wlr_set_startup_id = false

src/desktop.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <wlr/config.h>
-#include <wlr/types/wlr_box.h>
+#include <wlr/types/wlr_compositor.h>
 #include <wlr/types/wlr_cursor.h>
 #include <wlr/types/wlr_data_control_v1.h>
 #include <wlr/types/wlr_export_dmabuf_v1.h>
@@ -30,6 +30,7 @@
 #include <wlr/types/wlr_xdg_output_v1.h>
 #include <wlr/types/wlr_xdg_output_v1.h>
 #include <wlr/types/wlr_xdg_shell.h>
+#include <wlr/util/box.h>
 #include <wlr/version.h>
 #include "cursor.h"
 #include "layers.h"

src/layer-surface.h
@@ -41,6 +41,7 @@ struct _PhocLayerSurface {
 
     struct wlr_box geo;
     enum zwlr_layer_shell_v1_layer layer;
+    bool mapped;
 };
 
 PhocLayerSurface *phoc_layer_surface_new (void);

src/layer_shell.c
@@ -12,9 +12,9 @@
 #include <string.h>
 #include <time.h>
 #include <wayland-server-core.h>
-#include <wlr/types/wlr_box.h>
 #include <wlr/types/wlr_surface.h>
 #include <wlr/types/wlr_layer_shell_v1.h>
+#include <wlr/util/box.h>
 #include "cursor.h"
 #include "desktop.h"
 #include "layers.h"
@@ -183,7 +183,7 @@ static void arrange_layer(struct wlr_output *output,
 		}
 		if (box.width < 0 || box.height < 0) {
 			// TODO: Bubble up a protocol error?
-			wlr_layer_surface_v1_close(layer);
+			wlr_layer_surface_v1_destroy(layer);
 			continue;
 		}
 
@@ -195,7 +195,9 @@ static void arrange_layer(struct wlr_output *output,
 					state->margin.top, state->margin.right,
 					state->margin.bottom, state->margin.left);
 		}
-		wlr_layer_surface_v1_configure(layer, box.width, box.height);
+
+		if (box.width != old_geo.width || box.height != old_geo.height)
+			wlr_layer_surface_v1_configure(layer, box.width, box.height);
 
 		// Having a cursor newly end up over the moved layer will not
 		// automatically send a motion event to the surface. The event needs to
@@ -240,9 +242,9 @@ static void change_osk(const struct osk_origin *osk, struct wl_list layers[LAYER
 		wl_list_insert(&layers[ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY], &osk->surface->link);
 	}
 
-	if (!force_overlay && osk->layer != osk->surface->layer_surface->client_pending.layer) {
+	if (!force_overlay && osk->layer != osk->surface->layer_surface->pending.layer) {
 		wl_list_remove(&osk->surface->link);
-		wl_list_insert(&layers[osk->surface->layer_surface->client_pending.layer], &osk->surface->link);
+		wl_list_insert(&layers[osk->surface->layer_surface->pending.layer], &osk->surface->link);
 	}
 }
 
@@ -269,7 +271,7 @@ phoc_layer_shell_arrange (PhocOutput *output)
       PhocSeat *seat = PHOC_SEAT (elem->data);
 
       g_assert (PHOC_IS_SEAT (seat));
-      if (seat->focused_layer && seat->focused_layer->client_pending.layer >= osk_place.surface->layer_surface->client_pending.layer) {
+      if (seat->focused_layer && seat->focused_layer->pending.layer >= osk_place.surface->layer_surface->pending.layer) {
         osk_force_overlay = true;
         break;
       }
@@ -340,7 +342,7 @@ static void handle_output_destroy(struct wl_listener *listener, void *data) {
 		wl_container_of(listener, layer, output_destroy);
 	layer->layer_surface->output = NULL;
 	wl_list_remove(&layer->output_destroy.link);
-	wlr_layer_surface_v1_close(layer->layer_surface);
+	wlr_layer_surface_v1_destroy(layer->layer_surface);
 }
 
 static void handle_surface_commit(struct wl_listener *listener, void *data) {
@@ -353,16 +355,20 @@ static void handle_surface_commit(struct wl_listener *listener, void *data) {
 		PhocOutput *output = wlr_output->data;
 		struct wlr_box old_geo = layer->geo;
 
-		bool layer_changed = layer->layer != layer_surface->current.layer;
-		if (layer_changed) {
-			wl_list_remove(&layer->link);
-			wl_list_insert(&output->layers[layer_surface->current.layer],
-				&layer->link);
-			layer->layer = layer_surface->current.layer;
-		}
+		bool layer_changed = false;
+		if (layer_surface->current.committed != 0 || layer->mapped != layer_surface->mapped) {
+			layer->mapped = layer_surface->mapped;
+			layer_changed = layer->layer != layer_surface->current.layer;
+			if (layer_changed) {
+				wl_list_remove(&layer->link);
+				wl_list_insert(&output->layers[layer_surface->current.layer],
+					       &layer->link);
+				layer->layer = layer_surface->current.layer;
+			}
 
-		phoc_layer_shell_arrange (output);
-		phoc_layer_shell_update_focus ();
+			phoc_layer_shell_arrange (output);
+			phoc_layer_shell_update_focus ();
+		}
 
 		// Cursor changes which happen as a consequence of resizing a layer
 		// surface are applied in phoc_layer_shell_arrange. Because the resize happens
@@ -438,8 +444,8 @@ static void popup_unconstrain(struct roots_layer_popup *popup) {
 static void popup_damage(struct roots_layer_popup *layer_popup, bool whole) {
 	struct wlr_xdg_popup *popup = layer_popup->wlr_popup;
 	struct wlr_surface *surface = popup->base->surface;
-	int popup_sx = popup->geometry.x - popup->base->geometry.x;
-	int popup_sy = popup->geometry.y - popup->base->geometry.y;
+	int popup_sx = popup->geometry.x - popup->base->current.geometry.x;
+	int popup_sy = popup->geometry.y - popup->base->current.geometry.y;
 	int ox = popup_sx, oy = popup_sy;
 	PhocLayerSurface *layer;
 	while (layer_popup->parent_type == LAYER_PARENT_POPUP) {
@@ -493,13 +499,13 @@ static void popup_handle_map(struct wl_listener *listener, void *data) {
 	}
 
 	struct wlr_subsurface *child;
-	wl_list_for_each(child, &popup->wlr_popup->base->surface->subsurfaces_below, parent_link) {
+	wl_list_for_each(child, &popup->wlr_popup->base->surface->current.subsurfaces_below, current.link) {
 		struct roots_layer_subsurface *new_subsurface = layer_subsurface_create(child);
 		new_subsurface->parent_type = LAYER_PARENT_POPUP;
 		new_subsurface->parent_popup = popup;
 		wl_list_insert(&popup->subsurfaces, &new_subsurface->link);
 	}
-	wl_list_for_each(child, &popup->wlr_popup->base->surface->subsurfaces_above, parent_link) {
+	wl_list_for_each(child, &popup->wlr_popup->base->surface->current.subsurfaces_above, current.link) {
 		struct roots_layer_subsurface *new_subsurface = layer_subsurface_create(child);
 		new_subsurface->parent_type = LAYER_PARENT_POPUP;
 		new_subsurface->parent_popup = popup;
@@ -620,13 +626,13 @@ static void subsurface_handle_map(struct wl_listener *listener, void *data) {
 	struct roots_layer_subsurface *subsurface = wl_container_of(listener, subsurface, map);
 
 	struct wlr_subsurface *child;
-	wl_list_for_each(child, &subsurface->wlr_subsurface->surface->subsurfaces_below, parent_link) {
+	wl_list_for_each(child, &subsurface->wlr_subsurface->surface->current.subsurfaces_below, current.link) {
 		struct roots_layer_subsurface *new_subsurface = layer_subsurface_create(child);
 		new_subsurface->parent_type = LAYER_PARENT_SUBSURFACE;
 		new_subsurface->parent_subsurface = subsurface;
 		wl_list_insert(&subsurface->subsurfaces, &new_subsurface->link);
 	}
-	wl_list_for_each(child, &subsurface->wlr_subsurface->surface->subsurfaces_above, parent_link) {
+	wl_list_for_each(child, &subsurface->wlr_subsurface->surface->current.subsurfaces_above, current.link) {
 		struct roots_layer_subsurface *new_subsurface = layer_subsurface_create(child);
 		new_subsurface->parent_type = LAYER_PARENT_SUBSURFACE;
 		new_subsurface->parent_subsurface = subsurface;
@@ -707,13 +713,13 @@ static void handle_map(struct wl_listener *listener, void *data) {
 	}
 
 	struct wlr_subsurface *subsurface;
-	wl_list_for_each(subsurface, &layer_surface->surface->subsurfaces_below, parent_link) {
+	wl_list_for_each(subsurface, &layer_surface->surface->current.subsurfaces_below, current.link) {
 		struct roots_layer_subsurface *roots_subsurface = layer_subsurface_create(subsurface);
 		roots_subsurface->parent_type = LAYER_PARENT_LAYER;
 		roots_subsurface->parent_layer = layer;
 		wl_list_insert(&layer->subsurfaces, &roots_subsurface->link);
 	}
-	wl_list_for_each(subsurface, &layer_surface->surface->subsurfaces_above, parent_link) {
+	wl_list_for_each(subsurface, &layer_surface->surface->current.subsurfaces_above, current.link) {
 		struct roots_layer_subsurface *roots_subsurface = layer_subsurface_create(subsurface);
 		roots_subsurface->parent_type = LAYER_PARENT_LAYER;
 		roots_subsurface->parent_layer = layer;
@@ -751,14 +757,14 @@ void handle_layer_shell_surface(struct wl_listener *listener, void *data) {
 		wl_container_of(listener, desktop, layer_shell_surface);
 	g_debug ("new layer surface: namespace %s layer %d anchor %d "
 			"size %dx%d margin %d,%d,%d,%d",
-		layer_surface->namespace, layer_surface->client_pending.layer,
-		layer_surface->client_pending.anchor,
-		layer_surface->client_pending.desired_width,
-		layer_surface->client_pending.desired_height,
-		layer_surface->client_pending.margin.top,
-		layer_surface->client_pending.margin.right,
-		layer_surface->client_pending.margin.bottom,
-		layer_surface->client_pending.margin.left);
+		layer_surface->namespace, layer_surface->pending.layer,
+		layer_surface->pending.anchor,
+		layer_surface->pending.desired_width,
+		layer_surface->pending.desired_height,
+		layer_surface->pending.margin.top,
+		layer_surface->pending.margin.right,
+		layer_surface->pending.margin.bottom,
+		layer_surface->pending.margin.left);
 
 	if (!layer_surface->output) {
 		PhocInput *input = server->input;
@@ -778,7 +784,7 @@ void handle_layer_shell_surface(struct wl_listener *listener, void *data) {
 		if (output) {
 			layer_surface->output = output;
 		} else {
-			wlr_layer_surface_v1_close(layer_surface);
+			wlr_layer_surface_v1_destroy(layer_surface);
 			return;
 		}
 	}
@@ -811,12 +817,12 @@ void handle_layer_shell_surface(struct wl_listener *listener, void *data) {
 	layer_surface->data = roots_surface;
 
 	PhocOutput *output = layer_surface->output->data;
-	wl_list_insert(&output->layers[layer_surface->client_pending.layer], &roots_surface->link);
+	wl_list_insert(&output->layers[layer_surface->pending.layer], &roots_surface->link);
 
-	// Temporarily set the layer's current state to client_pending
+	// Temporarily set the layer's current state to pending
 	// So that we can easily arrange it
 	struct wlr_layer_surface_v1_state old_state = layer_surface->current;
-	layer_surface->current = layer_surface->client_pending;
+	layer_surface->current = layer_surface->pending;
 
 	phoc_layer_shell_arrange (output);
 	phoc_layer_shell_update_focus ();

src/layers.h
@@ -3,9 +3,9 @@
 #include "gesture.h"
 
 #include <stdbool.h>
-#include <wlr/types/wlr_box.h>
 #include <wlr/types/wlr_surface.h>
 #include <wlr/types/wlr_layer_shell_v1.h>
+#include <wlr/util/box.h>
 #include "output.h"
 #include "layer-surface.h"
 

src/output.c
@@ -85,7 +85,7 @@ get_surface_box (PhocOutputSurfaceIteratorData *data,
 
   struct wlr_box rotated_box;
 
-  wlr_box_rotated_bounds (&rotated_box, &box, data->rotation);
+  phoc_utils_rotated_bounds (&rotated_box, &box, data->rotation);
 
   struct wlr_box output_box = {0};
 
@@ -95,7 +95,7 @@ get_surface_box (PhocOutputSurfaceIteratorData *data,
 
   struct wlr_box intersection;
 
-  return wlr_box_intersection (&intersection, &output_box, &rotated_box);
+  return wlr_box_intersection (&intersection, &output_box, &box);
 }
 
 
@@ -614,9 +614,9 @@ phoc_output_layer_handle_surface (PhocOutput *self, PhocLayerSurface *layer_surf
 
     double popup_sx, popup_sy;
     popup_sx = layer_surface->geo.x;
-    popup_sx += popup->popup->geometry.x - popup->geometry.x;
+    popup_sx += popup->popup->geometry.x - popup->current.geometry.x;
     popup_sy = layer_surface->geo.y;
-    popup_sy += popup->popup->geometry.y - popup->geometry.y;
+    popup_sy += popup->popup->geometry.y - popup->current.geometry.y;
 
     phoc_output_xdg_surface_for_each_surface (self, popup,
                                               popup_sx, popup_sy, iterator, user_data);
@@ -822,7 +822,7 @@ damage_surface_iterator (PhocOutput *self, struct wlr_surface *surface, struct
   }
 
   if (*whole) {
-    wlr_box_rotated_bounds (&box, &box, rotation);
+    phoc_utils_rotated_bounds (&box, &box, rotation);
     wlr_output_damage_add_box (self->damage, &box);
   }
 

src/output.h
@@ -5,9 +5,9 @@
 #include <gio/gio.h>
 #include <glib-object.h>
 #include <wayland-server-core.h>
-#include <wlr/types/wlr_box.h>
 #include <wlr/types/wlr_output_damage.h>
 #include <wlr/types/wlr_xdg_shell.h>
+#include <wlr/util/box.h>
 
 G_BEGIN_DECLS
 

commit 06274c4efaa9bb2a27401e0fc4605d3a325852bb

xdg-shell: Use correct state for maximized and fullscreen flags
See https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/3199

Fixes: 6a508d5467395 ("Adjust to wlroots 0.15")

src/xdg_shell.c
@@ -202,7 +202,7 @@ static void handle_request_maximize(struct wl_listener *listener, void *data) {
 		return;
 	}
 
-	if (surface->toplevel->pending.maximized) {
+	if (surface->toplevel->requested.maximized) {
 		view_maximize(view, NULL);
 	} else {
 		view_restore(view);
@@ -366,10 +366,10 @@ void handle_xdg_shell_surface(struct wl_listener *listener, void *data) {
 		view_set_parent(&phoc_surface->view, &parent->view);
 	}
 
-	if (surface->toplevel->pending.maximized) {
+	if (surface->toplevel->requested.maximized) {
 		view_maximize(&phoc_surface->view, NULL);
 	}
-	phoc_view_set_fullscreen(&phoc_surface->view, surface->toplevel->pending.fullscreen,
+	phoc_view_set_fullscreen(&phoc_surface->view, surface->toplevel->requested.fullscreen,
 		surface->toplevel->requested.fullscreen_output);
 	view_auto_maximize(&phoc_surface->view);
 	view_set_title(&phoc_surface->view, surface->toplevel->title);