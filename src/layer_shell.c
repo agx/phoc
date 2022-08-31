agx/phoc

commit 4198fb6ec58e10eadd28f288f479c93c7ae76269

layer-shell: Update focus order in arrange_layers
Commit 0064831b7ce4bb6d03eab17fc2b95f59ae21979d changed the order of
surface rendering and input handling; however, focus handling also
needs to be adjusted.

Fixes squeekboard#109

src/layer_shell.c
@@ -300,7 +300,7 @@ void arrange_layers(struct roots_output *output) {
 	size_t nlayers = sizeof(layers_above_shell) / sizeof(layers_above_shell[0]);
 	struct roots_layer_surface *layer, *topmost = NULL;
 	for (size_t i = 0; i < nlayers; ++i) {
-		wl_list_for_each_reverse(layer,
+		wl_list_for_each(layer,
 				&output->layers[layers_above_shell[i]], link) {
 			if (layer->layer_surface->current.keyboard_interactive) {
 				topmost = layer;

commit 6db1949c5b2c87fef46825644b32d645f1188aa2

layer-shell: Adjust focus order to match rendering and input
In commit 4198fb6ec58e focus order has been updated, but wrongly - instead
of actually making it match the rendering and input order, it simply
traded surfaces with no exclusive zones being broken with surfaces with
exclusive zones being broken. Fix that by actually handling both cases
correctly.

Fixes: 4198fb6ec58e ("layer-shell: Update focus order in arrange_layers")
Closes: https://gitlab.gnome.org/World/Phosh/phosh/-/issues/793

src/layer_shell.c
@@ -309,8 +309,28 @@ phoc_layer_shell_update_focus (void)
   PhocOutput *output;
   wl_list_for_each (output, &server->desktop->outputs, link) {
     for (size_t i = 0; i < G_N_ELEMENTS(layers_above_shell); ++i) {
-      wl_list_for_each(layer_surface, &output->layer_surfaces, link) {
+      wl_list_for_each_reverse (layer_surface, &output->layer_surfaces, link) {
         if (layer_surface->layer != layers_above_shell[i])
+          continue;
+
+        if (layer_surface->layer_surface->current.exclusive_zone <= 0)
+          continue;
+
+        if (layer_surface->layer_surface->current.keyboard_interactive &&
+            layer_surface->layer_surface->mapped) {
+          topmost = layer_surface;
+          break;
+        }
+      }
+
+      if (topmost != NULL)
+        break;
+
+      wl_list_for_each (layer_surface, &output->layer_surfaces, link) {
+        if (layer_surface->layer != layers_above_shell[i])
+          continue;
+
+        if (layer_surface->layer_surface->current.exclusive_zone > 0)
           continue;
 
         if (layer_surface->layer_surface->current.keyboard_interactive &&