e0d830305d77beadbe5a66afb3f8996e1faf1bfe
Guido Günther <agx@sigxcpu.org>
2023-01-03 17:35:29 +0100

    Move "master" to "main"
    See #256

--- src/virtual.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <wayland-server-core.h>
-
-void phoc_handle_virtual_keyboard(struct wl_listener *listener, void *data);
-void phoc_handle_virtual_pointer(struct wl_listener *listener, void *data);
