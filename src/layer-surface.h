agx/phoc

commit afae4a3a26a17f81350c5bba181343740ab2536f

output: Use a single list for all layer surfaces
This has the advantage that we can use a single loop in most cases and
moving surfaces between layers keeps the ordering intact.

Performance wise it doesn't make much of a difference as the lists are
small.

This also simplifies the API as we pass output and wanted layer around
rather than per layer lists.

src/output.h
@@ -7,6 +7,7 @@
 #include <gio/gio.h>
 #include <glib-object.h>
 #include <wayland-server-core.h>
+#include <wlr/types/wlr_layer_shell_v1.h>
 #include <wlr/types/wlr_output_damage.h>
 #include <wlr/types/wlr_xdg_shell.h>
 #include <wlr/util/box.h>
@@ -37,7 +38,7 @@ struct _PhocOutput {
   struct wl_list            link; // PhocDesktop::outputs
 
   PhocView                 *fullscreen_view;
-  struct wl_list            layers[4]; // layer_surface::link
+  struct wl_list            layer_surfaces; // layer_surface::link
   bool                      force_shell_reveal;
 
   struct wlr_output_damage *damage;
@@ -84,7 +85,7 @@ void        phoc_output_drag_icons_for_each_surface  (PhocOutput *self,
                                                       PhocSurfaceIterator iterator,
                                                       void *user_data);
 void        phoc_output_layer_for_each_surface       (PhocOutput *self,
-                                                      struct wl_list *layer_surfaces,
+                                                      enum zwlr_layer_shell_v1_layer layer,
                                                       PhocSurfaceIterator iterator,
                                                       void *user_data);
 #ifdef PHOC_XWAYLAND
@@ -123,6 +124,7 @@ gboolean    phoc_output_is_match (PhocOutput *self,
                                   const char *model,
                                   const char *serial);
 gboolean    phoc_output_has_fullscreen_view (PhocOutput *self);
+gboolean    phoc_output_has_layer (PhocOutput *self, enum zwlr_layer_shell_v1_layer layer);
 
 guint       phoc_output_add_frame_callback   (PhocOutput        *self,
                                               PhocAnimatable    *animatable,

src/cursor.c
@@ -305,7 +305,10 @@ phoc_handle_shell_reveal (struct wlr_surface *surface, double lx, double ly, int
   PhocLayerSurface *layer_surface;
   bool left = false, right = false, top = false, bottom = false;
 
-  wl_list_for_each (layer_surface, &output->layers[ZWLR_LAYER_SHELL_V1_LAYER_TOP], link) {
+  wl_list_for_each (layer_surface, &output->layer_surfaces, link) {
+    if (layer_surface->layer != ZWLR_LAYER_SHELL_V1_LAYER_TOP)
+      continue;
+
     struct wlr_layer_surface_v1 *wlr_layer_surface = layer_surface->layer_surface;
     struct wlr_layer_surface_v1_state *state = &wlr_layer_surface->current;
     const uint32_t both_horiz = ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT
@@ -1099,8 +1102,11 @@ phoc_cursor_handle_touch_motion (PhocCursor                    *self,
       struct wlr_box *output_box = wlr_output_layout_get_box (desktop->layout, wlr_output);
 
       PhocLayerSurface *layer_surface;
-      wl_list_for_each_reverse (layer_surface, &phoc_output->layers[wlr_layer_surface->current.layer], link)
+      wl_list_for_each_reverse (layer_surface, &phoc_output->layer_surfaces, link)
       {
+        if (layer_surface->layer != wlr_layer_surface->current.layer)
+          continue;
+
         if (layer_surface->layer_surface->surface == root) {
           sx = lx - layer_surface->geo.x - output_box->x;
           sy = ly - layer_surface->geo.y - output_box->y;
@@ -1109,8 +1115,11 @@ phoc_cursor_handle_touch_motion (PhocCursor                    *self,
         }
       }
       // try the overlay layer as well since the on-screen keyboard might have been elevated there
-      wl_list_for_each_reverse (layer_surface, &phoc_output->layers[ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY], link)
+      wl_list_for_each_reverse (layer_surface, &phoc_output->layer_surfaces, link)
       {
+        if (layer_surface->layer != ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY)
+          continue;
+
         if (layer_surface->layer_surface->surface == root) {
           sx = lx - layer_surface->geo.x - output_box->x;
           sy = ly - layer_surface->geo.y - output_box->y;

src/desktop.c
@@ -160,11 +160,19 @@ static PhocView *desktop_view_at(PhocDesktop *desktop,
 }
 
 static struct wlr_surface *
-layer_surface_at (struct wl_list *layer, double ox, double oy, double *sx, double *sy)
+layer_surface_at (PhocOutput                     *output,
+                  enum zwlr_layer_shell_v1_layer  layer,
+                  double                          ox,
+                  double                          oy,
+                  double                         *sx,
+                  double                         *sy)
 {
   PhocLayerSurface *layer_surface;
 
-  wl_list_for_each_reverse(layer_surface, layer, link) {
+  wl_list_for_each_reverse(layer_surface, &output->layer_surfaces, link) {
+    if (layer_surface->layer != layer)
+      continue;
+
     if (layer_surface->layer_surface->current.exclusive_zone <= 0)
       continue;
 
@@ -178,7 +186,10 @@ layer_surface_at (struct wl_list *layer, double ox, double oy, double *sx, doubl
       return sub;
   }
 
-  wl_list_for_each(layer_surface, layer, link) {
+  wl_list_for_each(layer_surface, &output->layer_surfaces, link) {
+    if (layer_surface->layer != layer)
+      continue;
+
     if (layer_surface->layer_surface->current.exclusive_zone > 0)
       continue;
 
@@ -223,10 +234,10 @@ struct wlr_surface *phoc_desktop_surface_at(PhocDesktop *desktop,
 	}
 
 	if (wlr_output) {
-		phoc_output = wlr_output->data;
+		phoc_output = PHOC_OUTPUT (wlr_output->data);
 		wlr_output_layout_output_coords(desktop->layout, wlr_output, &ox, &oy);
 
-		if ((surface = layer_surface_at(&phoc_output->layers[ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY],
+		if ((surface = layer_surface_at(phoc_output, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY,
 						ox, oy, sx, sy))) {
 			return surface;
 		}
@@ -235,7 +246,7 @@ struct wlr_surface *phoc_desktop_surface_at(PhocDesktop *desktop,
 		if (output != NULL && output->fullscreen_view != NULL) {
 
 			if (output->force_shell_reveal) {
-				if ((surface = layer_surface_at(&phoc_output->layers[ZWLR_LAYER_SHELL_V1_LAYER_TOP],
+				if ((surface = layer_surface_at(phoc_output, ZWLR_LAYER_SHELL_V1_LAYER_TOP,
 								ox, oy, sx, sy))) {
 					return surface;
 				}
@@ -251,7 +262,7 @@ struct wlr_surface *phoc_desktop_surface_at(PhocDesktop *desktop,
 			}
 		}
 
-		if ((surface = layer_surface_at(&phoc_output->layers[ZWLR_LAYER_SHELL_V1_LAYER_TOP],
+		if ((surface = layer_surface_at(phoc_output, ZWLR_LAYER_SHELL_V1_LAYER_TOP,
 						ox, oy, sx, sy))) {
 			return surface;
 		}
@@ -266,11 +277,11 @@ struct wlr_surface *phoc_desktop_surface_at(PhocDesktop *desktop,
 	}
 
 	if (wlr_output) {
-		if ((surface = layer_surface_at(&phoc_output->layers[ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM],
+		if ((surface = layer_surface_at(phoc_output, ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM,
 						ox, oy, sx, sy))) {
 			return surface;
 		}
-		if ((surface = layer_surface_at(&phoc_output->layers[ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND],
+		if ((surface = layer_surface_at(phoc_output, ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND,
 						ox, oy, sx, sy))) {
 			return surface;
 		}

src/layer_shell.c
@@ -116,7 +116,7 @@ static void update_cursors(PhocLayerSurface *layer_surface, GSList *seats /* Pho
 
 static void arrange_layer(PhocOutput *output,
 		GSList *seats /* PhocSeat */,
-		struct wl_list *list /* PhocLayerSurface */,
+		enum zwlr_layer_shell_v1_layer layer,
 		struct wlr_box *usable_area, bool exclusive) {
 	PhocLayerSurface *layer_surface;
 	struct wlr_box full_area = { 0 };
@@ -124,9 +124,13 @@ static void arrange_layer(PhocOutput *output,
 	g_assert (PHOC_IS_OUTPUT (output));
 	wlr_output_effective_resolution(output->wlr_output,
 			&full_area.width, &full_area.height);
-	wl_list_for_each_reverse(layer_surface, list, link) {
+	wl_list_for_each_reverse(layer_surface, &output->layer_surfaces, link) {
 		struct wlr_layer_surface_v1 *wlr_layer_surface = layer_surface->layer_surface;
 		struct wlr_layer_surface_v1_state *state = &wlr_layer_surface->current;
+
+		if (layer_surface->layer != layer)
+		  continue;
+
 		if (exclusive != (state->exclusive_zone > 0)) {
 			continue;
 		}
@@ -214,42 +218,28 @@ static void arrange_layer(PhocOutput *output,
 	}
 }
 
-struct osk_origin {
-	struct wlr_layer_surface_v1_state state;
-	PhocLayerSurface *surface;
-	enum zwlr_layer_shell_v1_layer layer;
-};
+static PhocLayerSurface *
+find_osk (PhocOutput *output)
+{
+  PhocLayerSurface *layer_surface;
 
-static struct osk_origin find_osk(struct wl_list layers[LAYER_SHELL_LAYER_COUNT]) {
-	struct osk_origin origin = {0};
-	for (unsigned i = 0; i < LAYER_SHELL_LAYER_COUNT; i++) {
-		struct wl_list *list = &layers[i];
-		PhocLayerSurface *layer_surface;
-		wl_list_for_each(layer_surface, list, link) {
-			if (strcmp(layer_surface->layer_surface->namespace, "osk") == 0) {
-				origin.state = layer_surface->layer_surface->current;
-				origin.surface = layer_surface;
-				origin.layer = i;
-				return origin;
-			}
-		}
-	}
-	return origin;
+  wl_list_for_each(layer_surface, &output->layer_surfaces, link) {
+    if (strcmp(layer_surface->layer_surface->namespace, "osk") == 0)
+      return layer_surface;
+  }
+
+  return NULL;
 }
 
 /// Adjusts keyboard properties
 static void
-change_osk (const struct osk_origin *osk, struct wl_list layers[LAYER_SHELL_LAYER_COUNT], bool force_overlay)
+change_osk (PhocLayerSurface *osk, struct wl_list layer_surfaces, bool force_overlay)
 {
-  if (force_overlay && osk->layer != ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY) {
-    wl_list_remove(&osk->surface->link);
-    wl_list_insert(&layers[ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY], &osk->surface->link);
-  }
+  if (force_overlay && osk->layer != ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY)
+    osk->layer = ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY;
 
-	if (!force_overlay && osk->layer != osk->surface->layer_surface->pending.layer) {
-		wl_list_remove(&osk->surface->link);
-		wl_list_insert(&layers[osk->surface->layer_surface->pending.layer], &osk->surface->link);
-	}
+  if (!force_overlay && osk->layer != osk->layer_surface->pending.layer)
+    osk->layer = osk->layer_surface->pending.layer;
 }
 
 void
@@ -267,25 +257,25 @@ phoc_layer_shell_arrange (PhocOutput *output)
 
   wlr_output_effective_resolution (output->wlr_output, &usable_area.width, &usable_area.height);
 
-  struct osk_origin osk_place = find_osk (output->layers);
-  if (osk_place.surface) {
+  PhocLayerSurface *osk = find_osk (output);
+  if (osk) {
     bool osk_force_overlay = false;
 
     for (GSList *elem = seats; elem; elem = elem->next) {
       PhocSeat *seat = PHOC_SEAT (elem->data);
 
       g_assert (PHOC_IS_SEAT (seat));
-      if (seat->focused_layer && seat->focused_layer->pending.layer >= osk_place.surface->layer_surface->pending.layer) {
+      if (seat->focused_layer && seat->focused_layer->pending.layer >= osk->layer_surface->pending.layer) {
         osk_force_overlay = true;
         break;
       }
     }
-    change_osk (&osk_place, output->layers, osk_force_overlay);
+    change_osk (osk, output->layer_surfaces, osk_force_overlay);
   }
 
   // Arrange exclusive surfaces from top->bottom
   for (size_t i = 0; i < G_N_ELEMENTS(layers); ++i)
-    arrange_layer (output, seats, &output->layers[layers[i]], &usable_area, true);
+    arrange_layer (output, seats, layers[i], &usable_area, true);
   output->usable_area = usable_area;
 
   PhocView *view;
@@ -301,7 +291,7 @@ phoc_layer_shell_arrange (PhocOutput *output)
 
   // Arrange non-exlusive surfaces from top->bottom
   for (size_t i = 0; i < G_N_ELEMENTS(layers); ++i)
-    arrange_layer (output, seats, &output->layers[layers[i]], &usable_area, false);
+    arrange_layer (output, seats, layers[i], &usable_area, false);
 }
 
 void
@@ -322,7 +312,10 @@ phoc_layer_shell_update_focus (void)
         if (layers_above_shell[i] == ZWLR_LAYER_SHELL_V1_LAYER_TOP)
           continue;
       }
-      wl_list_for_each(layer_surface, &output->layers[layers_above_shell[i]], link) {
+      wl_list_for_each(layer_surface, &output->layer_surfaces, link) {
+        if (layer_surface->layer != layers_above_shell[i])
+          continue;
+
         if (layer_surface->layer_surface->current.keyboard_interactive &&
             layer_surface->layer_surface->mapped) {
           topmost = layer_surface;
@@ -355,13 +348,8 @@ static void handle_surface_commit(struct wl_listener *listener, void *data) {
 		if (wlr_layer_surface->current.committed != 0 || layer_surface->mapped != wlr_layer_surface->mapped) {
 			layer_surface->mapped = wlr_layer_surface->mapped;
 			layer_changed = layer_surface->layer != wlr_layer_surface->current.layer;
-			if (layer_changed) {
-				wl_list_remove(&layer_surface->link);
-				wl_list_insert(&output->layers[wlr_layer_surface->current.layer],
-					       &layer_surface->link);
-				layer_surface->layer = wlr_layer_surface->current.layer;
-			}
 
+			layer_surface->layer = wlr_layer_surface->current.layer;
 			phoc_layer_shell_arrange (output);
 			phoc_layer_shell_update_focus ();
 		}
@@ -803,7 +791,7 @@ void handle_layer_shell_surface(struct wl_listener *listener, void *data) {
 	wlr_layer_surface->data = layer_surface;
 
 	PhocOutput *output = wlr_layer_surface->output->data;
-	wl_list_insert(&output->layers[wlr_layer_surface->pending.layer], &layer_surface->link);
+	wl_list_insert(&output->layer_surfaces, &layer_surface->link);
 
 	// Temporarily set the layer's current state to pending
 	// So that we can easily arrange it

src/output.c
@@ -424,8 +424,7 @@ phoc_output_initable_init (GInitable    *initable,
   self->damage_destroy.notify = phoc_output_damage_handle_destroy;
   wl_signal_add (&self->damage->events.destroy, &self->damage_destroy);
 
-  for (size_t i = 0; i < G_N_ELEMENTS (self->layers); ++i)
-    wl_list_init (&self->layers[i]);
+  wl_list_init (&self->layer_surfaces);
 
   PhocOutputConfig *output_config = phoc_config_get_output (config, self->wlr_output);
 
@@ -505,8 +504,7 @@ phoc_output_finalize (GObject *object)
   g_clear_slist (&priv->frame_callbacks,
                  (GDestroyNotify)phoc_output_frame_callback_info_free);
 
-  for (size_t i = 0; i < G_N_ELEMENTS (self->layers); ++i)
-    wl_list_init (&self->layers[i]);
+  wl_list_init (&self->layer_surfaces);
 
   g_clear_object (&priv->shield);
   g_clear_object (&self->desktop);
@@ -759,19 +757,25 @@ phoc_output_layer_handle_surface (PhocOutput *self, PhocLayerSurface *layer_surf
  */
 void
 phoc_output_layer_for_each_surface (PhocOutput          *self,
-                                    struct wl_list      *layer_surfaces,
+                                    enum zwlr_layer_shell_v1_layer layer,
                                     PhocSurfaceIterator  iterator,
                                     void                *user_data)
 {
   PhocLayerSurface *layer_surface;
 
-  wl_list_for_each_reverse (layer_surface, layer_surfaces, link)
+  wl_list_for_each_reverse (layer_surface, &self->layer_surfaces, link)
   {
+    if (layer_surface->layer != layer)
+      continue;
+
     if (layer_surface->layer_surface->current.exclusive_zone <= 0) {
       phoc_output_layer_handle_surface (self, layer_surface, iterator, user_data);
     }
   }
-  wl_list_for_each (layer_surface, layer_surfaces, link) {
+  wl_list_for_each (layer_surface, &self->layer_surfaces, link) {
+    if (layer_surface->layer != layer)
+      continue;
+
     if (layer_surface->layer_surface->current.exclusive_zone > 0) {
       phoc_output_layer_handle_surface (self, layer_surface, iterator, user_data);
     }
@@ -861,9 +865,9 @@ phoc_output_for_each_surface (PhocOutput          *self,
   phoc_output_drag_icons_for_each_surface (self, server->input,
                                            iterator, user_data);
 
-  for (size_t i = 0; i < G_N_ELEMENTS (self->layers); ++i) {
-    phoc_output_layer_for_each_surface (self, &self->layers[i],
-                                        iterator, user_data);
+  for (enum zwlr_layer_shell_v1_layer layer = ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND;
+       layer <= ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY; layer++) {
+    phoc_output_layer_for_each_surface (self, layer, iterator, user_data);
   }
 }
 
@@ -1322,4 +1326,26 @@ phoc_output_raise_shield (PhocOutput *self)
     priv->shield = phoc_output_shield_new (self);
 
   phoc_output_shield_raise (priv->shield);
+}
+
+/**
+ * phoc_output_has_layer:
+ * @self: The #PhocOutput
+ *
+ * Returns: %TRUE if the output has a Phoc.LayerSurface in the given layer.
+ *          %FALSE otherwise.
+ */
+gboolean
+phoc_output_has_layer (PhocOutput *self, enum zwlr_layer_shell_v1_layer layer)
+{
+  PhocLayerSurface *layer_surface;
+
+  g_assert (PHOC_IS_OUTPUT (self));
+
+  wl_list_for_each (layer_surface, &self->layer_surfaces, link) {
+    if (layer_surface->layer == layer)
+      return TRUE;
+  }
+
+  return FALSE;
 }

src/render.c
@@ -321,14 +321,12 @@ render_layer (PhocOutput                     *output,
               pixman_region32_t              *damage,
               enum zwlr_layer_shell_v1_layer  layer)
 {
-  struct wl_list *layer_surfaces = &output->layers[layer];
-
   struct render_data data = {
     .damage = damage,
     .alpha = 1.0f,
   };
 
-  phoc_output_layer_for_each_surface(output, layer_surfaces, render_surface_iterator, &data);
+  phoc_output_layer_for_each_surface(output, layer, render_surface_iterator, &data);
 }
 
 static void count_surface_iterator(PhocOutput *output,
@@ -352,9 +350,8 @@ static bool scan_out_fullscreen_view(PhocOutput *output) {
 		}
 	}
 
-	if (!wl_list_empty(&output->layers[ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY])) {
+	if (phoc_output_has_layer (output, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY))
 		return false;
-	}
 
 	struct wlr_output_cursor *cursor;
 	wl_list_for_each(cursor, &wlr_output->cursors, link) {

commit 75ff514c6062d74eb3052db167ae152433178df8

layer-surface: Fix names for output list
The list members are PhocLayerSurface and on the output it's
called layer_surfaces.

Fixes: afae4a3a ("output: Use a single list for all layer surfaces")

src/layer-surface.h
@@ -28,7 +28,7 @@ struct _PhocLayerSurface {
     GObject parent;
 
     struct wlr_layer_surface_v1 *layer_surface;
-    struct wl_list link; // PhocOutput::layers[current]
+    struct wl_list link; // PhocOutput::layer_surfaces
 
     struct wl_listener destroy;
     struct wl_listener map;

src/output.h
@@ -38,7 +38,7 @@ struct _PhocOutput {
   struct wl_list            link; // PhocDesktop::outputs
 
   PhocView                 *fullscreen_view;
-  struct wl_list            layer_surfaces; // layer_surface::link
+  struct wl_list            layer_surfaces; // PhocLayerSurface::link
 
   struct wlr_output_damage *damage;
   GList                    *debug_touch_points;