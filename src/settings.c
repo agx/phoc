8c1ca23be5dc50b86e65a4385361490dfa0c5b06
Guido Günther <agx@sigxcpu.org>
2023-04-13 17:18:12 +0200

    settings: Reindent
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/444>

--- src/settings.c
+++ src/settings.c
@@ -15,163 +15,166 @@
 #include "settings.h"
 #include "utils.h"
 
-static bool parse_modeline(const char *s, drmModeModeInfo *mode) {
-	char hsync[16];
-	char vsync[16];
-	float fclock;
+static bool
+parse_modeline (const char *s, drmModeModeInfo *mode)
+{
+  char hsync[16];
+  char vsync[16];
+  float fclock;
 
-	mode->type = DRM_MODE_TYPE_USERDEF;
+  mode->type = DRM_MODE_TYPE_USERDEF;
 
-	if (sscanf(s, "%f %hd %hd %hd %hd %hd %hd %hd %hd %15s %15s",
-		   &fclock,
-		   &mode->hdisplay,
-		   &mode->hsync_start,
-		   &mode->hsync_end,
-		   &mode->htotal,
-		   &mode->vdisplay,
-		   &mode->vsync_start,
-		   &mode->vsync_end,
-		   &mode->vtotal, hsync, vsync) != 11) {
-		return false;
-	}
+  if (sscanf (s, "%f %hd %hd %hd %hd %hd %hd %hd %hd %15s %15s",
+              &fclock,
+              &mode->hdisplay,
+              &mode->hsync_start,
+              &mode->hsync_end,
+              &mode->htotal,
+              &mode->vdisplay,
+              &mode->vsync_start,
+              &mode->vsync_end,
+              &mode->vtotal, hsync, vsync) != 11) {
+    return false;
+  }
 
-	mode->clock = fclock * 1000;
-	mode->vrefresh = mode->clock * 1000.0 * 1000.0
-		/ mode->htotal / mode->vtotal;
-	if (strcasecmp(hsync, "+hsync") == 0) {
-		mode->flags |= DRM_MODE_FLAG_PHSYNC;
-	} else if (strcasecmp(hsync, "-hsync") == 0) {
-		mode->flags |= DRM_MODE_FLAG_NHSYNC;
-	} else {
-		return false;
-	}
+  mode->clock = fclock * 1000;
+  mode->vrefresh = mode->clock * 1000.0 * 1000.0
+                   / mode->htotal / mode->vtotal;
+  if (strcasecmp (hsync, "+hsync") == 0) {
+    mode->flags |= DRM_MODE_FLAG_PHSYNC;
+  } else if (strcasecmp (hsync, "-hsync") == 0) {
+    mode->flags |= DRM_MODE_FLAG_NHSYNC;
+  } else {
+    return false;
+  }
 
-	if (strcasecmp(vsync, "+vsync") == 0) {
-		mode->flags |= DRM_MODE_FLAG_PVSYNC;
-	} else if (strcasecmp(vsync, "-vsync") == 0) {
-		mode->flags |= DRM_MODE_FLAG_NVSYNC;
-	} else {
-		return false;
-	}
+  if (strcasecmp (vsync, "+vsync") == 0) {
+    mode->flags |= DRM_MODE_FLAG_PVSYNC;
+  } else if (strcasecmp (vsync, "-vsync") == 0) {
+    mode->flags |= DRM_MODE_FLAG_NVSYNC;
+  } else {
+    return false;
+  }
 
-	snprintf(mode->name, sizeof(mode->name), "%dx%d@%d",
-		 mode->hdisplay, mode->vdisplay, mode->vrefresh / 1000);
+  snprintf (mode->name, sizeof(mode->name), "%dx%d@%d",
+            mode->hdisplay, mode->vdisplay, mode->vrefresh / 1000);
 
-	return true;
+  return true;
 }
 
 static const char *output_prefix = "output:";
 
-static int config_ini_handler(PhocConfig *config, const char *section, const char *name,
-		const char *value) {
-	if (strcmp(section, "core") == 0) {
-		if (strcmp(name, "xwayland") == 0) {
-			if (strcasecmp(value, "true") == 0) {
-				config->xwayland = true;
-			} else if (strcasecmp(value, "immediate") == 0) {
-				config->xwayland = true;
-				config->xwayland_lazy = false;
-			} else if (strcasecmp(value, "false") == 0) {
-				config->xwayland = false;
-			} else {
-				g_critical ("got unknown xwayland value: %s", value);
-			}
-		} else {
-			g_critical ("got unknown core config: %s", name);
-		}
-	} else if (strncmp(output_prefix, section, strlen(output_prefix)) == 0) {
-		const char *output_name = section + strlen(output_prefix);
-		PhocOutputConfig *oc;
-		bool found = false;
+static int
+config_ini_handler (PhocConfig *config, const char *section, const char *name, const char *value)
+{
+  if (strcmp (section, "core") == 0) {
+    if (strcmp (name, "xwayland") == 0) {
+      if (strcasecmp (value, "true") == 0) {
+        config->xwayland = true;
+      } else if (strcasecmp (value, "immediate") == 0) {
+        config->xwayland = true;
+        config->xwayland_lazy = false;
+      } else if (strcasecmp (value, "false") == 0) {
+        config->xwayland = false;
+      } else {
+        g_critical ("got unknown xwayland value: %s", value);
+      }
+    } else {
+      g_critical ("got unknown core config: %s", name);
+    }
+  } else if (strncmp (output_prefix, section, strlen (output_prefix)) == 0) {
+    const char *output_name = section + strlen (output_prefix);
+    PhocOutputConfig *oc;
+    bool found = false;
 
-		wl_list_for_each(oc, &config->outputs, link) {
-			if (strcmp(oc->name, output_name) == 0) {
-				found = true;
-				break;
-			}
-		}
+    wl_list_for_each (oc, &config->outputs, link) {
+      if (strcmp (oc->name, output_name) == 0) {
+        found = true;
+        break;
+      }
+    }
 
-		if (!found) {
-			oc = g_new0 (PhocOutputConfig, 1);
-			oc->name = strdup(output_name);
-			oc->transform = WL_OUTPUT_TRANSFORM_NORMAL;
-			oc->scale = 0;
-			oc->enable = true;
-			wl_list_init(&oc->modes);
-			wl_list_insert(&config->outputs, &oc->link);
-		}
+    if (!found) {
+      oc = g_new0 (PhocOutputConfig, 1);
+      oc->name = strdup (output_name);
+      oc->transform = WL_OUTPUT_TRANSFORM_NORMAL;
+      oc->scale = 0;
+      oc->enable = true;
+      wl_list_init (&oc->modes);
+      wl_list_insert (&config->outputs, &oc->link);
+    }
 
-		if (strcmp(name, "enable") == 0) {
-			if (strcasecmp(value, "true") == 0) {
-				oc->enable = true;
-			} else if (strcasecmp(value, "false") == 0) {
-				oc->enable = false;
-			} else {
-				g_critical ("got invalid output enable value: %s", value);
-			}
-		} else if (strcmp(name, "x") == 0) {
-			oc->x = strtol(value, NULL, 10);
-		} else if (strcmp(name, "y") == 0) {
-			oc->y = strtol(value, NULL, 10);
-		} else if (strcmp(name, "scale") == 0) {
-			if (strcmp(value, "auto") == 0) {
-				oc->scale = 0;
-			} else {
-				oc->scale = strtof(value, NULL);
-				g_assert (oc->scale > 0);
-			}
-		} else if (strcmp(name, "rotate") == 0) {
-			if (strcmp(value, "normal") == 0) {
-				oc->transform = WL_OUTPUT_TRANSFORM_NORMAL;
-			} else if (strcmp(value, "90") == 0) {
-				oc->transform = WL_OUTPUT_TRANSFORM_90;
-			} else if (strcmp(value, "180") == 0) {
-				oc->transform = WL_OUTPUT_TRANSFORM_180;
-			} else if (strcmp(value, "270") == 0) {
-				oc->transform = WL_OUTPUT_TRANSFORM_270;
-			} else if (strcmp(value, "flipped") == 0) {
-				oc->transform = WL_OUTPUT_TRANSFORM_FLIPPED;
-			} else if (strcmp(value, "flipped-90") == 0) {
-				oc->transform = WL_OUTPUT_TRANSFORM_FLIPPED_90;
-			} else if (strcmp(value, "flipped-180") == 0) {
-				oc->transform = WL_OUTPUT_TRANSFORM_FLIPPED_180;
-			} else if (strcmp(value, "flipped-270") == 0) {
-				oc->transform = WL_OUTPUT_TRANSFORM_FLIPPED_270;
-			} else {
-				g_critical ("got unknown transform value: %s", value);
-			}
-			/* Make sure we rotate clockwise */
-			phoc_utils_fix_transform(&oc->transform);
-		} else if (strcmp(name, "mode") == 0) {
-			char *end;
-			oc->mode.width = strtol(value, &end, 10);
-			g_assert (*end == 'x');
-			++end;
-			oc->mode.height = strtol(end, &end, 10);
-			if (*end) {
-				g_assert (*end == '@');
-				++end;
-				oc->mode.refresh_rate = strtof(end, &end);
-				g_assert (strcmp("Hz", end) == 0);
-			}
-			g_debug ("Configured output %s with mode %dx%d@%f",
-					oc->name, oc->mode.width, oc->mode.height,
-					oc->mode.refresh_rate);
-		} else if (strcmp(name, "modeline") == 0) {
-			PhocOutputModeConfig *mode = g_new0 (PhocOutputModeConfig, 1);
+    if (strcmp (name, "enable") == 0) {
+      if (strcasecmp (value, "true") == 0) {
+        oc->enable = true;
+      } else if (strcasecmp (value, "false") == 0) {
+        oc->enable = false;
+      } else {
+        g_critical ("got invalid output enable value: %s", value);
+      }
+    } else if (strcmp (name, "x") == 0) {
+      oc->x = strtol (value, NULL, 10);
+    } else if (strcmp (name, "y") == 0) {
+      oc->y = strtol (value, NULL, 10);
+    } else if (strcmp (name, "scale") == 0) {
+      if (strcmp (value, "auto") == 0) {
+        oc->scale = 0;
+      } else {
+        oc->scale = strtof (value, NULL);
+        g_assert (oc->scale > 0);
+      }
+    } else if (strcmp (name, "rotate") == 0) {
+      if (strcmp (value, "normal") == 0) {
+        oc->transform = WL_OUTPUT_TRANSFORM_NORMAL;
+      } else if (strcmp (value, "90") == 0) {
+        oc->transform = WL_OUTPUT_TRANSFORM_90;
+      } else if (strcmp (value, "180") == 0) {
+        oc->transform = WL_OUTPUT_TRANSFORM_180;
+      } else if (strcmp (value, "270") == 0) {
+        oc->transform = WL_OUTPUT_TRANSFORM_270;
+      } else if (strcmp (value, "flipped") == 0) {
+        oc->transform = WL_OUTPUT_TRANSFORM_FLIPPED;
+      } else if (strcmp (value, "flipped-90") == 0) {
+        oc->transform = WL_OUTPUT_TRANSFORM_FLIPPED_90;
+      } else if (strcmp (value, "flipped-180") == 0) {
+        oc->transform = WL_OUTPUT_TRANSFORM_FLIPPED_180;
+      } else if (strcmp (value, "flipped-270") == 0) {
+        oc->transform = WL_OUTPUT_TRANSFORM_FLIPPED_270;
+      } else {
+        g_critical ("got unknown transform value: %s", value);
+      }
+      /* Make sure we rotate clockwise */
+      phoc_utils_fix_transform (&oc->transform);
+    } else if (strcmp (name, "mode") == 0) {
+      char *end;
+      oc->mode.width = strtol (value, &end, 10);
+      g_assert (*end == 'x');
+      ++end;
+      oc->mode.height = strtol (end, &end, 10);
+      if (*end) {
+        g_assert (*end == '@');
+        ++end;
+        oc->mode.refresh_rate = strtof (end, &end);
+        g_assert (strcmp ("Hz", end) == 0);
+      }
+      g_debug ("Configured output %s with mode %dx%d@%f",
+               oc->name, oc->mode.width, oc->mode.height,
+               oc->mode.refresh_rate);
+    } else if (strcmp (name, "modeline") == 0) {
+      PhocOutputModeConfig *mode = g_new0 (PhocOutputModeConfig, 1);
 
-			if (parse_modeline(value, &mode->info)) {
-				wl_list_insert(&oc->modes, &mode->link);
-			} else {
-				g_free(mode);
-				g_critical ("Invalid modeline: %s", value);
-			}
-		}
-	} else {
-		g_critical ("Found unknown config section: %s", section);
-	}
+      if (parse_modeline (value, &mode->info)) {
+        wl_list_insert (&oc->modes, &mode->link);
+      } else {
+        g_free (mode);
+        g_critical ("Invalid modeline: %s", value);
+      }
+    }
+  } else {
+    g_critical ("Found unknown config section: %s", section);
+  }
 
-	return 1;
+  return 1;
 }
 
 
@@ -191,16 +194,16 @@ phoc_config_create (const char *config_path)
 
   config->xwayland = true;
   config->xwayland_lazy = true;
-  wl_list_init(&config->outputs);
+  wl_list_init (&config->outputs);
 
   config->config_path = g_strdup (config_path);
 
   if (!config->config_path) {
     // get the config path from the current directory
     char cwd[MAXPATHLEN];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    if (getcwd (cwd, sizeof(cwd)) != NULL) {
       char buf[MAXPATHLEN];
-      if (snprintf(buf, MAXPATHLEN, "%s/%s", cwd, "phoc.ini") >= MAXPATHLEN) {
+      if (snprintf (buf, MAXPATHLEN, "%s/%s", cwd, "phoc.ini") >= MAXPATHLEN) {
         g_critical ("config path too long");
         return NULL;
       }
@@ -233,7 +236,7 @@ phoc_config_create (const char *config_path)
       continue;
     }
 
-    for (int j = 0; j < g_strv_length (keys); j++)  {
+    for (int j = 0; j < g_strv_length (keys); j++) {
       const char *key = keys[j];
       g_autofree char *value = NULL;
 
@@ -249,7 +252,7 @@ phoc_config_create (const char *config_path)
     }
   }
 
- out:
+out:
   config->keybindings = phoc_keybindings_new ();
 
   return config;
@@ -262,12 +265,17 @@ phoc_config_create (const char *config_path)
  *
  * Destroy the config and free its resources.
  */
-void phoc_config_destroy (PhocConfig *config)
+void
+phoc_config_destroy (PhocConfig *config)
 {
   PhocOutputConfig *oc, *otmp = NULL;
-  wl_list_for_each_safe(oc, otmp, &config->outputs, link) {
+
+  wl_list_for_each_safe (oc, otmp, &config->outputs, link)
+  {
     PhocOutputModeConfig *omc, *omctmp = NULL;
-    wl_list_for_each_safe(omc, omctmp, &oc->modes, link) {
+
+    wl_list_for_each_safe (omc, omctmp, &oc->modes, link)
+    {
       g_free (omc);
     }
     g_free (oc->name);
@@ -292,13 +300,14 @@ PhocOutputConfig *
 phoc_config_get_output (PhocConfig *config, struct wlr_output *output)
 {
   char name[88];
-  snprintf(name, sizeof(name), "%s %s %s", output->make, output->model,
-           output->serial);
-
   PhocOutputConfig *oc;
-  wl_list_for_each(oc, &config->outputs, link) {
-    if (strcmp(oc->name, output->name) == 0 ||
-        strcmp(oc->name, name) == 0) {
+
+  snprintf (name, sizeof(name), "%s %s %s", output->make, output->model,
+            output->serial);
+
+  wl_list_for_each (oc, &config->outputs, link) {
+    if (strcmp (oc->name, output->name) == 0 ||
+        strcmp (oc->name, name) == 0) {
       return oc;
     }
   }
--- src/settings.h
+++ src/settings.h
@@ -12,38 +12,38 @@ G_BEGIN_DECLS
 #define PHOC_CONFIG_DEFAULT_SEAT_NAME "seat0"
 
 typedef struct _PhocOutputModeConfig {
-	drmModeModeInfo info;
-	struct wl_list link;
+  drmModeModeInfo info;
+  struct wl_list  link;
 } PhocOutputModeConfig;
 
 typedef struct _PhocOutputConfig {
-	char *name;
-	bool enable;
-	enum wl_output_transform transform;
-	int x, y;
-	float scale;
-	struct wl_list link;
-	struct Mode {
-		int width, height;
-		float refresh_rate;
-	} mode;
-	struct wl_list modes;
+  char                    *name;
+  bool                     enable;
+  enum wl_output_transform transform;
+  int                      x, y;
+  float                    scale;
+  struct wl_list           link;
+  struct Mode {
+    int   width, height;
+    float refresh_rate;
+  } mode;
+  struct wl_list           modes;
 } PhocOutputConfig;
 
 typedef struct _PhocConfig {
-	bool xwayland;
-	bool xwayland_lazy;
+  bool             xwayland;
+  bool             xwayland_lazy;
 
-	PhocKeybindings *keybindings;
+  PhocKeybindings *keybindings;
 
-	struct wl_list outputs;
+  struct wl_list   outputs;
 
-	char *config_path;
+  char            *config_path;
 } PhocConfig;
 
-PhocConfig       *phoc_config_create (const char *config_path);
-void              phoc_config_destroy (PhocConfig *config);
-PhocOutputConfig *phoc_config_get_output (PhocConfig *config,
+PhocConfig       *phoc_config_create     (const char *config_path);
+void              phoc_config_destroy    (PhocConfig *config);
+PhocOutputConfig *phoc_config_get_output (PhocConfig        *config,
                                           struct wlr_output *output);
 
 G_END_DECLS
