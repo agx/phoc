agx/phoc

commit 75e8003587c9c80716863e8c4ea3f935454bd88a

server: Deduplicate XWayland config
Having it as flag and config option was duplication. Now that we have
pulled config parsing out of the server itself we can deduplicate it.

Fixes 15373e454a75f4415888a807e0f251a6c5c2f27a

Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/444>

src/server.h
@@ -27,7 +27,6 @@ G_DECLARE_FINAL_TYPE (PhocServer, phoc_server, PHOC, SERVER, GObject);
 typedef enum _PhocServerFlags {
   PHOC_SERVER_FLAG_NONE       = 0,
   PHOC_SERVER_FLAG_SHELL_MODE = 1 << 0,
-  PHOC_SERVER_FLAG_XWAYLAND   = 1 << 1,
 } PhocServerFlags;
 
 typedef enum _PhocServerDebugFlags {

src/desktop.c
@@ -598,7 +598,7 @@ phoc_desktop_setup_xwayland (PhocDesktop *self)
   self->xcursor_manager = wlr_xcursor_manager_create(NULL, PHOC_XCURSOR_SIZE);
   g_return_if_fail (self->xcursor_manager);
 
-  if (config->xwayland || (server->flags & PHOC_SERVER_FLAG_XWAYLAND)) {
+  if (config->xwayland) {
     self->xwayland = wlr_xwayland_create(server->wl_display,
 					 server->compositor, config->xwayland_lazy);
     if (!self->xwayland) {

src/main.c
@@ -154,12 +154,12 @@ main(int argc, char **argv)
 
   if (shell_mode)
     flags |= PHOC_SERVER_FLAG_SHELL_MODE;
-  if (xwayland)
-    flags |= PHOC_SERVER_FLAG_XWAYLAND;
 
   config = phoc_config_new_from_file (config_path);
   if (config == NULL)
     return EXIT_FAILURE;
+  if (xwayland)
+    config->xwayland = TRUE;
 
   loop = g_main_loop_new (NULL, FALSE);
   if (!phoc_server_setup (server, config, exec, loop, flags, debug_flags))

tests/test-xwayland.c
@@ -132,8 +132,14 @@ test_client_xwayland_server_prepare (PhocServer *server, gpointer data)
 static void
 test_xwayland_normal (void)
 {
+  PhocConfig *config = phoc_config_new_from_data (
+    "[core]\n"
+    "xwayland=true\n"
+    "[output:HEADLESS-1]\n"
+    "mode=1024x768\n"
+    );
   PhocTestClientIface iface = {
-    .server_flags   = PHOC_SERVER_FLAG_XWAYLAND,
+    .config         = config,
     .server_prepare = test_client_xwayland_server_prepare,
     .client_run     = test_client_xwayland_normal,
   };

tests/testlib.c
@@ -427,7 +427,7 @@ on_timer_expired (gpointer unused)
 void
 phoc_test_client_run (gint timeout, PhocTestClientIface *iface, gpointer data)
 {
-  PhocConfig *config = phoc_config_new_from_file (TEST_PHOC_INI);
+  PhocConfig *config;
   struct task_data td = { .data = data };
   g_autoptr(PhocServer) server = phoc_server_get_default ();
   g_autoptr(GMainLoop) loop = g_main_loop_new (NULL, FALSE);
@@ -437,10 +437,11 @@ phoc_test_client_run (gint timeout, PhocTestClientIface *iface, gpointer data)
   if (iface)
     td.func = iface->client_run;
 
+  config = (iface && iface->config) ? iface->config : phoc_config_new_from_file (TEST_PHOC_INI);
   g_assert_true (PHOC_IS_SERVER (server));
   g_assert_true (config);
   g_assert_true (phoc_server_setup(server, config, NULL, loop,
-                                   iface ? iface->server_flags : 0,
+                                   PHOC_SERVER_FLAG_NONE,
                                    PHOC_SERVER_DEBUG_FLAG_NONE));
   if (iface && iface->server_prepare)
     g_assert_true (iface->server_prepare(server, data));

tests/testlib.h
@@ -72,9 +72,9 @@ typedef gboolean (* PhocTestClientFunc) (PhocTestClientGlobals *globals, gpointe
 
 typedef struct PhocTestClientIface {
   /* Prepare function runs in server context */
-  PhocTestServerFunc server_prepare;
-  PhocTestClientFunc client_run;
-  PhocServerFlags    server_flags;
+  PhocTestServerFunc  server_prepare;
+  PhocTestClientFunc  client_run;
+  PhocConfig         *config;
 } PhocTestClientIface;
 
 typedef struct _PhocTestXdgToplevelSurface

commit 768df79ff2ada652df076bdb316a02165a125b98

server: Drop flag in docstring too
Fixes: 75e8003587c9c80716863e8c4ea3f935454bd88a

src/server.h
@@ -22,7 +22,6 @@ G_DECLARE_FINAL_TYPE (PhocServer, phoc_server, PHOC, SERVER, GObject);
  * PhocServerFlags:
  *
  * PHOC_SHELL_FLAG_SHELL_MODE: Expect a shell to attach
- * PHOC_SHELL_FLAG_XWAYLAND: Start XWayland
  */
 typedef enum _PhocServerFlags {
   PHOC_SERVER_FLAG_NONE       = 0,