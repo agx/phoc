39a844b4fb14cb7c6a3d4534ddef969aeeea7de4
Guido Günther <agx@sigxcpu.org>
2023-08-02 11:34:12 +0200

    pointer: Add missing G_{BEGIN,END}_DECLS
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/467>

--- src/pointer.h
+++ src/pointer.h
@@ -6,8 +6,12 @@
 #include <gio/gio.h>
 #include <glib-object.h>
 
+G_BEGIN_DECLS
+
 #define PHOC_TYPE_POINTER (phoc_pointer_get_type ())
 
 G_DECLARE_FINAL_TYPE (PhocPointer, phoc_pointer, PHOC, POINTER, PhocInputDevice);
 
 PhocPointer *phoc_pointer_new (struct wlr_input_device *device, PhocSeat *seat);
+
+G_END_DECLS
