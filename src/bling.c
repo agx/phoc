56135838ba5eba345e679e14d43ef5396ad03387
Guido Günther <agx@sigxcpu.org>
2023-08-24 20:25:24 +0200

    Add an interface to render blings
    To attach some graphical sugar in various spaces of the compositor
    add an interface the objects can implement to get rendered.
    
    This will allow us to have e.g. additional rects or quads in various
    places when needed. We can combine this nicely with PhocTimedAnimation.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/478>

---
+++ src/bling.c
@@ -0,0 +1,98 @@
+/* Copyright (C) 2023 Guido Günther
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#define G_LOG_DOMAIN "phoc-bling"
+
+#include "bling.h"
+
+/**
+ * PhocBling:
+ *
+ * A #PhocBling is additional render bling such as rectangles or other
+ * elements to be drawn by the compositor.
+ *
+ * Blings are currently meant to be attached to [type@View] but can be
+ * extended to other objects.
+ */
+
+G_DEFINE_INTERFACE (PhocBling, phoc_bling, G_TYPE_OBJECT)
+
+
+static void
+phoc_bling_default_init (PhocBlingInterface *iface)
+{
+}
+
+
+void
+phoc_bling_render (PhocBling *self, PhocOutput *output)
+{
+  PhocBlingInterface *iface;
+
+  g_assert (PHOC_IS_BLING (self));
+
+  iface = PHOC_BLING_GET_IFACE (self);
+  g_assert (iface->render);
+
+  iface->render (self, output);
+}
+
+
+PhocBox
+phoc_bling_get_box (PhocBling *self)
+{
+  PhocBlingInterface *iface;
+
+  g_assert (PHOC_IS_BLING (self));
+
+  iface = PHOC_BLING_GET_IFACE (self);
+  g_assert (iface->get_box);
+
+  return iface->get_box (self);
+}
+
+
+void
+phoc_bling_map (PhocBling *self)
+{
+  PhocBlingInterface *iface;
+
+  g_assert (PHOC_IS_BLING (self));
+
+  iface = PHOC_BLING_GET_IFACE (self);
+  g_assert (iface->map);
+
+  iface->map (self);
+}
+
+
+void
+phoc_bling_unmap (PhocBling *self)
+{
+  PhocBlingInterface *iface;
+
+  g_assert (PHOC_IS_BLING (self));
+
+  iface = PHOC_BLING_GET_IFACE (self);
+  g_assert (iface->unmap);
+
+  iface->unmap (self);
+}
+
+
+gboolean
+phoc_bling_is_mapped (PhocBling *self)
+{
+  PhocBlingInterface *iface;
+
+  g_assert (PHOC_IS_BLING (self));
+
+  iface = PHOC_BLING_GET_IFACE (self);
+  g_assert (iface->is_mapped);
+
+  return iface->is_mapped (self);
+}
---
+++ src/bling.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2023 Guido Günther
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "phoc-types.h"
+#include "render.h"
+
+#include <glib-object.h>
+
+#pragma once
+
+G_BEGIN_DECLS
+
+#define PHOC_TYPE_BLING (phoc_bling_get_type())
+
+G_DECLARE_INTERFACE (PhocBling, phoc_bling, PHOC, BLING, GObject)
+
+struct _PhocBlingInterface
+{
+  GTypeInterface parent_iface;
+
+  /**
+   * PhocBlingInterface::get_box:
+   * @self: A bilng
+   *
+   * Get a minimal box in layout coordindates that contains the bling
+   *
+   * Returns: The box
+   */
+  PhocBox       (*get_box)    (PhocBling *self);
+  /**
+   * PhocBlingInterface::render:
+   * @self: A bling
+   *
+   * Render the bling. Scissoring is handled by the renderer prior to invoking
+   * this function.
+   */
+  void          (*render)     (PhocBling *self, PhocOutput *output);
+  /**
+   * PhocBlingInterface::map:
+   * @self: A bling
+   *
+   * Map the bling so it can be rendered.
+   */
+  void          (*map)        (PhocBling *self);
+  /**
+   * PhocBlingInterface::unmap:
+   * @self: A bling
+   *
+   * Umap the bling so it's not rendered anymore
+   */
+  void          (*unmap)      (PhocBling *self);
+  /**
+   * PhocBlingInterface::is_mapped:
+   * @self: A bling
+   *
+   * Check whether the bling is mapped.
+   *
+   * Returns: %TRUE if the bling is mapped, otherwise %FALSE.
+   */
+   gboolean      (*is_mapped)  (PhocBling *self);
+};
+
+void                    phoc_bling_render                        (PhocBling    *self,
+                                                                  PhocOutput   *render);
+PhocBox                 phoc_bling_get_box                       (PhocBling    *self);
+void                    phoc_bling_map                           (PhocBling    *self);
+void                    phoc_bling_unmap                         (PhocBling    *self);
+gboolean                phoc_bling_is_mapped                     (PhocBling    *self);
+
+G_END_DECLS
