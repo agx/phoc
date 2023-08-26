10825c7bc0358bcceb50cf436c46ed254208e117
Guido Günther <agx@sigxcpu.org>
2023-08-26 16:17:20 +0200

    view-private: Rename first argument to self
    We don't have everything fixed up in view.c but let's keep new
    headers cosistent.
    
    Gbp-Dch: Ignore
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/480>

--- src/view-private.h
+++ src/view-private.h
@@ -11,15 +11,15 @@
 G_BEGIN_DECLS
 
 /* Functions to be used by derived classes only */
-void             view_set_title                      (PhocView *view, const char *title);
-void             view_set_parent                     (PhocView *view, PhocView *parent);
-void             phoc_view_setup                     (PhocView *view);
-void             view_send_frame_done_if_not_visible (PhocView *view);
-void             view_update_position                (PhocView *view, int x, int y);
-void             view_update_size                    (PhocView *view, int width, int height);
-void             phoc_view_set_initial_focus         (PhocView *view);
-void             phoc_view_map                       (PhocView *view, struct wlr_surface *surface);
-void             phoc_view_unmap                     (PhocView *view);
-void             phoc_view_apply_damage              (PhocView *view);
+void             view_set_title                      (PhocView *self, const char *title);
+void             view_set_parent                     (PhocView *self, PhocView *parent);
+void             phoc_view_setup                     (PhocView *self);
+void             view_send_frame_done_if_not_visible (PhocView *self);
+void             view_update_position                (PhocView *self, int x, int y);
+void             view_update_size                    (PhocView *self, int width, int height);
+void             phoc_view_set_initial_focus         (PhocView *self);
+void             phoc_view_map                       (PhocView *self, struct wlr_surface *surface);
+void             phoc_view_unmap                     (PhocView *self);
+void             phoc_view_apply_damage              (PhocView *self);
 
 G_END_DECLS
