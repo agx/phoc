agx/phoc

commit 3d78879210019a7e268c418e727a4b245e1c0d56

Update to wlroots 0.13 and drop support for older versions

src/output.c
@@ -223,9 +223,9 @@ phoc_output_handle_mode (struct wl_listener *listener, void *data)
 }
 
 static void
-phoc_output_handle_transform (struct wl_listener *listener, void *data)
+phoc_output_handle_commit (struct wl_listener *listener, void *data)
 {
-  PhocOutput *self = wl_container_of (listener, self, transform);
+  PhocOutput *self = wl_container_of (listener, self, commit);
 
   phoc_layer_shell_arrange (self);
 }
@@ -296,8 +296,8 @@ phoc_output_constructed (GObject *object)
   wl_signal_add (&self->wlr_output->events.enable, &self->enable);
   self->mode.notify = phoc_output_handle_mode;
   wl_signal_add (&self->wlr_output->events.mode, &self->mode);
-  self->transform.notify = phoc_output_handle_transform;
-  wl_signal_add (&self->wlr_output->events.transform, &self->transform);
+  self->commit.notify = phoc_output_handle_commit;
+  wl_signal_add (&self->wlr_output->events.commit, &self->commit);
 
   self->damage_frame.notify = phoc_output_damage_handle_frame;
   wl_signal_add (&self->damage->events.frame, &self->damage_frame);
@@ -376,7 +376,7 @@ phoc_output_finalize (GObject *object)
   wl_list_remove (&self->output_destroy.link);
   wl_list_remove (&self->enable.link);
   wl_list_remove (&self->mode.link);
-  wl_list_remove (&self->transform.link);
+  wl_list_remove (&self->commit.link);
   wl_list_remove (&self->damage_frame.link);
   wl_list_remove (&self->damage_destroy.link);
   g_list_free_full (self->debug_touch_points, g_free);

debian/control
@@ -16,7 +16,7 @@ Build-Depends:
  wayland-protocols,
  libgirepository1.0-dev <!nodoc>,
  libxcb1-dev,
- libwlroots-dev (>= 0.12.0) <!pkg.phoc.embedwlroots>,
+ libwlroots-dev (>= 0.13.0) <!pkg.phoc.embedwlroots>,
  python3-jinja2 <!nodoc>,
  python3-pygments <!nodoc>,
  python3-toml <!nodoc>,

meson.build
@@ -65,7 +65,7 @@ if not embed_wlroots.disabled() and wlroots_proj.found()
         have_wlr_set_startup_id = true
         have_wlr_remove_startup_info = true
 else
-        wlroots = dependency('wlroots', version: '>= 0.12.0')
+        wlroots = dependency('wlroots', version: '>= 0.13.0')
         wlroots_has_xwayland = cc.get_define('WLR_HAS_XWAYLAND', prefix: '#include <wlr/config.h>', dependencies: wlroots) == '1'
         wlroots_has_output_power_management = cc.has_header('wlr/types/wlr_output_power_management_v1.h', dependencies: wlroots)
         have_wlr_set_startup_id = false

protocols/wlr-layer-shell-unstable-v1.xml
@@ -25,7 +25,7 @@
     THIS SOFTWARE.
   </copyright>
 
-  <interface name="zwlr_layer_shell_v1" version="3">
+  <interface name="zwlr_layer_shell_v1" version="4">
     <description summary="create surfaces that are layers of the desktop">
       Clients can use this interface to assign the surface_layer role to
       wl_surfaces. Such surfaces are assigned to a "layer" of the output and
@@ -46,6 +46,12 @@
         or committed is a client error, and any attempts by a client to attach
         or manipulate a buffer prior to the first layer_surface.configure call
         must also be treated as errors.
+
+        After creating a layer_surface object and setting it up, the client
+        must perform an initial commit without any buffer attached.
+        The compositor will reply with a layer_surface.configure event.
+        The client must acknowledge it and is then allowed to attach a buffer
+        to map the surface.
 
         You may pass NULL for output to allow the compositor to decide which
         output to use. Generally this will be the one that the user most
@@ -94,7 +100,7 @@
     </request>
   </interface>
 
-  <interface name="zwlr_layer_surface_v1" version="3">
+  <interface name="zwlr_layer_surface_v1" version="4">
     <description summary="layer metadata interface">
       An interface that may be implemented by a wl_surface, for surfaces that
       are designed to be rendered as a layer of a stacked desktop-like
@@ -103,6 +109,14 @@
       Layer surface state (layer, size, anchor, exclusive zone,
       margin, interactivity) is double-buffered, and will be applied at the
       time wl_surface.commit of the corresponding wl_surface is called.
+
+      Attaching a null buffer to a layer surface unmaps it.
+
+      Unmapping a layer_surface means that the surface cannot be shown by the
+      compositor until it is explicitly mapped again. The layer_surface
+      returns to the state it had right after layer_shell.get_layer_surface.
+      The client can re-map the surface by performing a commit without any
+      buffer attached, waiting for a configure event and handling it as usual.
     </description>
 
     <request name="set_size">
@@ -189,21 +203,85 @@
       <arg name="left" type="int"/>
     </request>
 
+    <enum name="keyboard_interactivity">
+      <description summary="types of keyboard interaction possible for a layer shell surface">
+        Types of keyboard interaction possible for layer shell surfaces. The
+        rationale for this is twofold: (1) some applications are not interested
+        in keyboard events and not allowing them to be focused can improve the
+        desktop experience; (2) some applications will want to take exclusive
+        keyboard focus.
+      </description>
+
+      <entry name="none" value="0">
+        <description summary="no keyboard focus is possible">
+          This value indicates that this surface is not interested in keyboard
+          events and the compositor should never assign it the keyboard focus.
+
+          This is the default value, set for newly created layer shell surfaces.
+
+          This is useful for e.g. desktop widgets that display information or
+          only have interaction with non-keyboard input devices.
+        </description>
+      </entry>
+      <entry name="exclusive" value="1">
+        <description summary="request exclusive keyboard focus">
+          Request exclusive keyboard focus if this surface is above the shell surface layer.
+
+          For the top and overlay layers, the seat will always give
+          exclusive keyboard focus to the top-most layer which has keyboard
+          interactivity set to exclusive. If this layer contains multiple
+          surfaces with keyboard interactivity set to exclusive, the compositor
+          determines the one receiving keyboard events in an implementation-
+          defined manner. In this case, no guarantee is made when this surface
+          will receive keyboard focus (if ever).
+
+          For the bottom and background layers, the compositor is allowed to use
+          normal focus semantics.
+
+          This setting is mainly intended for applications that need to ensure
+          they receive all keyboard events, such as a lock screen or a password
+          prompt.
+        </description>
+      </entry>
+      <entry name="on_demand" value="2" since="4">
+        <description summary="request regular keyboard focus semantics">
+          This requests the compositor to allow this surface to be focused and
+          unfocused by the user in an implementation-defined manner. The user
+          should be able to unfocus this surface even regardless of the layer
+          it is on.
+
+          Typically, the compositor will want to use its normal mechanism to
+          manage keyboard focus between layer shell surfaces with this setting
+          and regular toplevels on the desktop layer (e.g. click to focus).
+          Nevertheless, it is possible for a compositor to require a special
+          interaction to focus or unfocus layer shell surfaces (e.g. requiring
+          a click even if focus follows the mouse normally, or providing a
+          keybinding to switch focus between layers).
+
+          This setting is mainly intended for desktop shell components (e.g.
+          panels) that allow keyboard interaction. Using this option can allow
+          implementing a desktop shell that can be fully usable without the
+          mouse.
+        </description>
+      </entry>
+    </enum>
+
     <request name="set_keyboard_interactivity">
       <description summary="requests keyboard events">
-        Set to 1 to request that the seat send keyboard events to this layer
-        surface. For layers below the shell surface layer, the seat will use
-        normal focus semantics. For layers above the shell surface layers, the
-        seat will always give exclusive keyboard focus to the top-most layer
-        which has keyboard interactivity set to true.
+        Set how keyboard events are delivered to this surface. By default,
+        layer shell surfaces do not receive keyboard events; this request can
+        be used to change this.
+
+        This setting is inherited by child surfaces set by the get_popup
+        request.
 
         Layer surfaces receive pointer, touch, and tablet events normally. If
         you do not want to receive them, set the input region on your surface
         to an empty region.
 
-        Events is double-buffered, see wl_surface.commit.
+        Keyboard interactivity is double-buffered, see wl_surface.commit.
       </description>
-      <arg name="keyboard_interactivity" type="uint"/>
+      <arg name="keyboard_interactivity" type="uint" enum="keyboard_interactivity"/>
     </request>
 
     <request name="get_popup">
@@ -288,6 +366,7 @@
       <entry name="invalid_surface_state" value="0" summary="provided surface state is invalid"/>
       <entry name="invalid_size" value="1" summary="size is invalid"/>
       <entry name="invalid_anchor" value="2" summary="anchor bitfield is invalid"/>
+      <entry name="invalid_keyboard_interactivity" value="3" summary="keyboard interactivity is invalid"/>
     </enum>
 
     <enum name="anchor" bitfield="true">

src/cursor.h
@@ -7,6 +7,8 @@
 #pragma once
 
 #include <wlr/types/wlr_pointer_constraints_v1.h>
+#include <wlr/types/wlr_touch.h>
+#include <wlr/types/wlr_tablet_tool.h>
 #include "seat.h"
 
 #include <glib-object.h>

src/keyboard.c
@@ -123,15 +123,15 @@ keysym_is_modifier(xkb_keysym_t keysym)
 static void
 pressed_keysyms_update(xkb_keysym_t *pressed_keysyms,
                        const xkb_keysym_t *keysyms, size_t keysyms_len,
-                       enum wlr_key_state state)
+                       enum wl_keyboard_key_state state)
 {
   for (size_t i = 0; i < keysyms_len; ++i) {
     if (keysym_is_modifier(keysyms[i])) {
       continue;
     }
-    if (state == WLR_KEY_PRESSED) {
+    if (state == WL_KEYBOARD_KEY_STATE_PRESSED) {
       pressed_keysyms_add(pressed_keysyms, keysyms[i]);
-    } else { // WLR_KEY_RELEASED
+    } else { // WL_KEYBOARD_KEY_STATE_RELEASED
       pressed_keysyms_remove(pressed_keysyms, keysyms[i]);
     }
   }
@@ -300,7 +300,7 @@ phoc_keyboard_handle_key(PhocKeyboard *self,
                                             &modifiers);
   pressed_keysyms_update(self->pressed_keysyms_translated, keysyms,
                          keysyms_len, event->state);
-  if (event->state == WLR_KEY_PRESSED) {
+  if (event->state == WL_KEYBOARD_KEY_STATE_PRESSED) {
     handled = keyboard_execute_binding(self,
                                        self->pressed_keysyms_translated, modifiers, keysyms,
                                        keysyms_len);
@@ -310,13 +310,13 @@ phoc_keyboard_handle_key(PhocKeyboard *self,
   keysyms_len = keyboard_keysyms_raw(self, keycode, &keysyms, &modifiers);
   pressed_keysyms_update(self->pressed_keysyms_raw, keysyms, keysyms_len,
                          event->state);
-  if (event->state == WLR_KEY_PRESSED && !handled) {
+  if (event->state == WL_KEYBOARD_KEY_STATE_PRESSED && !handled) {
     handled = keyboard_execute_binding(self,
                                        self->pressed_keysyms_raw, modifiers, keysyms, keysyms_len);
   }
 
   // Handle subscribed keysyms
-  if (event->state == WLR_KEY_PRESSED && !handled) {
+  if (event->state == WL_KEYBOARD_KEY_STATE_PRESSED && !handled) {
     handled = keyboard_execute_subscribed_binding (self,
                                                    self->pressed_keysyms_raw, modifiers,
                                                    keysyms, keysyms_len, event->time_msec);

src/output.h
@@ -37,7 +37,7 @@ struct _PhocOutput {
 
   struct wl_listener        enable;
   struct wl_listener        mode;
-  struct wl_listener        transform;
+  struct wl_listener        commit;
   struct wl_listener        damage_frame;
   struct wl_listener        damage_destroy;
   struct wl_listener        output_destroy;

src/phosh-private.c
@@ -441,7 +441,7 @@ thumbnail_frame_handle_copy (struct wl_client   *wl_client,
   uint32_t renderer_flags = 0;
   if (!view_render_to_buffer (view, width, height, stride, &renderer_flags, data)) {
     wl_shm_buffer_end_access (frame->buffer);
-    zwlr_screencopy_frame_v1_send_failed (frame_resource);
+    zwlr_screencopy_frame_v1_send_failed (frame->resource);
     return;
   }
   enum zwlr_screencopy_frame_v1_flags flags = (renderer_flags & WLR_RENDERER_READ_PIXELS_Y_INVERT) ? ZWLR_SCREENCOPY_FRAME_V1_FLAGS_Y_INVERT : 0;

commit a6b3511f0eaadaee9e67843b8495d73ae0374fe3

output: Invoke arrange-layers less
phoc_layer_shell_arrange() needs to be called on mode, scale and
transform changes to adjust the layer surfaces and available space for
views. It doesn't need to be called when we only submit damage or attach
new buffers. This reduces the number of invocations (and hence used CPU
time).

Fixes: 3d788792 ("Update to wlroots 0.13 and drop support for older versions")
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/352>

src/output.c
@@ -391,11 +391,10 @@ phoc_output_handle_commit (struct wl_listener *listener, void *data)
   PhocOutput *self = wl_container_of (listener, self, commit);
   struct wlr_output_event_commit *event = data;
 
-  /* FIXME: We do this way too often */
-  phoc_layer_shell_arrange (self);
-
-  if (event->committed & (WLR_OUTPUT_STATE_TRANSFORM | WLR_OUTPUT_STATE_SCALE))
+  if (event->committed & (WLR_OUTPUT_STATE_TRANSFORM | WLR_OUTPUT_STATE_SCALE)) {
+    phoc_layer_shell_arrange (self);
     update_output_manager_config (self->desktop);
+  }
 }
 
 