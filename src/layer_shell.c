agx/phoc

commit 08504c40d63f2c2d5872cc76950477760562d374

view: Don't leak details of view arrangement
Instead of leaking the details how view are layed out keep that
local to view.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/522>

src/layer_shell.c
@@ -272,15 +272,8 @@ phoc_layer_shell_arrange (PhocOutput *output)
   output->usable_area = usable_area;
 
   PhocView *view;
-  wl_list_for_each (view, &output->desktop->views, link) {
-    if (phoc_view_is_maximized (view)) {
-      view_arrange_maximized (view, NULL);
-    } else if (phoc_view_is_tiled (view)) {
-      view_arrange_tiled (view, NULL);
-    } else if (output->desktop->maximize) {
-      view_center (view, NULL);
-    }
-  }
+  wl_list_for_each (view, &output->desktop->views, link)
+    phoc_view_arrange (view, NULL);
 
   // Arrange non-exlusive surfaces from top->bottom
   for (size_t i = 0; i < G_N_ELEMENTS(layers); ++i)

src/view.c
@@ -424,7 +424,7 @@ phoc_view_get_maximized_box (PhocView *self, PhocOutput *output, struct wlr_box 
 }
 
 
-void
+static void
 view_arrange_maximized (PhocView *self, PhocOutput *output)
 {
   PhocViewPrivate *priv;
@@ -504,7 +504,7 @@ phoc_view_get_tiled_box (PhocView               *self,
 }
 
 
-void
+static void
 view_arrange_tiled (PhocView *self, PhocOutput *output)
 {
   PhocViewPrivate *priv;
@@ -1092,15 +1092,8 @@ view_update_scale (PhocView *view)
     priv->scale = 1.0;
   }
 
-  if (priv->scale != oldscale) {
-    if (phoc_view_is_maximized(view)) {
-      view_arrange_maximized (view, NULL);
-    } else if (phoc_view_is_tiled (view)) {
-      view_arrange_tiled (view, NULL);
-    } else {
-      view_center (view, NULL);
-    }
-  }
+  if (priv->scale != oldscale)
+    phoc_view_arrange (view, NULL);
 }
 
 
@@ -2355,4 +2348,26 @@ phoc_view_get_blings (PhocView *self)
   priv = phoc_view_get_instance_private (self);
 
   return priv->blings;
+}
+
+
+/**
+ * phoc_view_arrange:
+ * @self: a view
+ * @output:(nullable): the output to arrange the view on
+ *
+ * Arrange a view based on it's current state (floating, tiled or maximized)
+ */
+void
+phoc_view_arrange (PhocView *self, PhocOutput *output)
+{
+  g_assert (PHOC_IS_VIEW (self));
+  g_assert (output == NULL || PHOC_IS_OUTPUT (output));
+
+  if (phoc_view_is_maximized (self))
+    view_arrange_maximized (self, output);
+  else if (phoc_view_is_tiled (self))
+    view_arrange_tiled (self, output);
+  else
+    view_center (self, output);
 }

src/view.h
@@ -166,8 +166,7 @@ gboolean phoc_view_is_maximized (PhocView *view);
 gboolean phoc_view_is_tiled (PhocView *view);
 gboolean phoc_view_is_fullscreen (PhocView *self);
 void view_update_decorated(PhocView *view, bool decorated);
-void view_arrange_maximized(PhocView *view, PhocOutput *output);
-void view_arrange_tiled(PhocView *view, PhocOutput *output);
+void phoc_view_arrange (PhocView *self, PhocOutput *output);
 void view_get_box(PhocView *view, struct wlr_box *box);
 void phoc_view_get_geometry (PhocView *self, struct wlr_box *box);
 void phoc_view_move (PhocView *self, double x, double y);

commit 2c6226e4bfd14ed202b98f8a70ede1556dfb5abd

layer-shell: Don't center views unconditionally
We only want to do so when using automaximize

Fixes: 08504c40 ("view: Don't leak details of view arrangement")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/530>

src/layer_shell.c
@@ -273,7 +273,7 @@ phoc_layer_shell_arrange (PhocOutput *output)
 
   PhocView *view;
   wl_list_for_each (view, &output->desktop->views, link)
-    phoc_view_arrange (view, NULL);
+    phoc_view_arrange (view, NULL, output->desktop->maximize);
 
   // Arrange non-exlusive surfaces from top->bottom
   for (size_t i = 0; i < G_N_ELEMENTS(layers); ++i)

src/view.c
@@ -677,7 +677,7 @@ phoc_view_move_to_next_output (PhocView *view, enum wlr_direction direction)
     return true;
   }
 
-  phoc_view_arrange (view, output);
+  phoc_view_arrange (view, output, TRUE);
   return true;
 }
 
@@ -1042,7 +1042,7 @@ view_update_scale (PhocView *view)
   }
 
   if (priv->scale != oldscale)
-    phoc_view_arrange (view, NULL);
+    phoc_view_arrange (view, NULL, TRUE);
 }
 
 
@@ -2290,11 +2290,12 @@ phoc_view_get_blings (PhocView *self)
  * phoc_view_arrange:
  * @self: a view
  * @output:(nullable): the output to arrange the view on
+ * @center: Whether to center the view as fallback
  *
  * Arrange a view based on it's current state (floating, tiled or maximized)
  */
 void
-phoc_view_arrange (PhocView *self, PhocOutput *output)
+phoc_view_arrange (PhocView *self, PhocOutput *output, gboolean center)
 {
   g_assert (PHOC_IS_VIEW (self));
   g_assert (output == NULL || PHOC_IS_OUTPUT (output));
@@ -2303,6 +2304,6 @@ phoc_view_arrange (PhocView *self, PhocOutput *output)
     view_arrange_maximized (self, output);
   else if (phoc_view_is_tiled (self))
     view_arrange_tiled (self, output);
-  else
+  else if (center)
     view_center (self, output);
 }

src/view.h
@@ -166,7 +166,7 @@ gboolean              phoc_view_is_maximized (PhocView *view);
 gboolean              phoc_view_is_tiled (PhocView *view);
 gboolean              phoc_view_is_fullscreen (PhocView *self);
 void                  phoc_view_update_decorated (PhocView *view, bool decorated);
-void                  phoc_view_arrange (PhocView *self, PhocOutput *output);
+void                  phoc_view_arrange (PhocView *self, PhocOutput *output, gboolean center);
 void                  phoc_view_get_box (PhocView *view, struct wlr_box *box);
 void                  phoc_view_get_geometry (PhocView *self, struct wlr_box *box);
 void                  phoc_view_move (PhocView *self, double x, double y);