7fcd32d82b49611451ad34572ed2e32749e5e75a
Guido Günther <agx@sigxcpu.org>
2024-02-26 11:24:06 +0100

    output: Use drag-icon getters
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/541>

--- src/output.c
+++ src/output.c
@@ -1262,9 +1262,9 @@ phoc_output_drag_icons_for_each_surface (PhocOutput          *self,
     if (!phoc_drag_icon_is_mapped (drag_icon))
       continue;
 
-    double ox = drag_icon->x - output_box.x;
-    double oy = drag_icon->y - output_box.y;
-    phoc_output_surface_for_each_surface (self, drag_icon->wlr_drag_icon->surface,
+    double ox = phoc_drag_icon_get_x (drag_icon) - output_box.x;
+    double oy = phoc_drag_icon_get_y (drag_icon) - output_box.y;
+    phoc_output_surface_for_each_surface (self, phoc_drag_icon_get_wlr_surface (drag_icon),
                                           ox, oy, iterator, user_data);
   }
 }
@@ -1454,8 +1454,10 @@ phoc_output_damage_whole_drag_icon (PhocOutput *self, PhocDragIcon *icon)
 {
   bool whole = true;
 
-  phoc_output_surface_for_each_surface (self, icon->wlr_drag_icon->surface,
-                                        icon->x, icon->y,
+  phoc_output_surface_for_each_surface (self,
+                                        phoc_drag_icon_get_wlr_surface (icon),
+                                        phoc_drag_icon_get_x (icon),
+                                        phoc_drag_icon_get_y (icon),
                                         damage_surface_iterator, &whole);
 }
 
