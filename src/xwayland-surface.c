agx/phoc

commit 1e9df0a2d828a63deba10e4c08899548de0cba42

treewide: Adjust to map logic changes
See sway commit c9e1dab3187d22ada0232d699e759a628e0ed185

Forwards to wlroots-git 2023-06-05

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/463>

src/xwayland-surface.c
@@ -42,6 +42,8 @@ typedef struct _PhocXWaylandSurface {
   struct wl_listener request_resize;
   struct wl_listener request_maximize;
   struct wl_listener request_fullscreen;
+  struct wl_listener associate;
+  struct wl_listener dissociate;
   struct wl_listener map;
   struct wl_listener unmap;
   struct wl_listener set_title;
@@ -476,6 +478,30 @@ handle_unmap (struct wl_listener *listener, void *data)
   view_unmap (view);
 }
 
+
+static void
+handle_associate (struct wl_listener *listener, void *data)
+{
+  PhocXWaylandSurface *self = wl_container_of (listener, self, associate);
+  struct wlr_xwayland_surface *xwayland_surface = self->xwayland_surface;
+
+  wl_signal_add (&xwayland_surface->surface->events.unmap, &self->unmap);
+  self->unmap.notify = handle_unmap;
+  wl_signal_add (&xwayland_surface->surface->events.map, &self->map);
+  self->map.notify = handle_map;
+}
+
+
+static void
+handle_dissociate (struct wl_listener *listener, void *data)
+{
+  PhocXWaylandSurface *self = wl_container_of (listener, self, dissociate);
+
+  wl_list_remove (&self->map.link);
+  wl_list_remove (&self->unmap.link);
+}
+
+
 /* }}} */
 
 
@@ -516,11 +542,11 @@ phoc_xwayland_surface_constructed (GObject *object)
   self->request_fullscreen.notify = handle_request_fullscreen;
   wl_signal_add(&surface->events.request_fullscreen, &self->request_fullscreen);
 
-  self->map.notify = handle_map;
-  wl_signal_add(&surface->events.map, &self->map);
+  self->associate.notify = handle_associate;
+  wl_signal_add (&surface->events.associate, &self->associate);
 
-  self->unmap.notify = handle_unmap;
-  wl_signal_add(&surface->events.unmap, &self->unmap);
+  self->dissociate.notify = handle_dissociate;
+  wl_signal_add (&surface->events.dissociate, &self->dissociate);
 
   self->set_title.notify = handle_set_title;
   wl_signal_add(&surface->events.set_title, &self->set_title);
@@ -530,6 +556,9 @@ phoc_xwayland_surface_constructed (GObject *object)
 
   self->set_startup_id.notify = handle_set_startup_id;
   wl_signal_add(&surface->events.set_startup_id, &self->set_startup_id);
+
+  wl_list_init (&self->map.link);
+  wl_list_init (&self->unmap.link);
 }
 
 
@@ -544,8 +573,8 @@ phoc_xwayland_surface_finalize (GObject *object)
   wl_list_remove(&self->request_resize.link);
   wl_list_remove(&self->request_maximize.link);
   wl_list_remove(&self->request_fullscreen.link);
-  wl_list_remove(&self->map.link);
-  wl_list_remove(&self->unmap.link);
+  wl_list_remove(&self->associate.link);
+  wl_list_remove(&self->dissociate.link);
   wl_list_remove(&self->set_title.link);
   wl_list_remove(&self->set_class.link);
   wl_list_remove(&self->set_startup_id.link);

src/layer-surface.c
@@ -121,7 +121,7 @@ phoc_layer_surface_finalize (GObject *object)
   PhocLayerSurface *self = PHOC_LAYER_SURFACE (object);
   PhocOutput *output = phoc_layer_surface_get_output (self);
 
-  if (self->layer_surface->mapped)
+  if (self->layer_surface->surface->mapped)
     phoc_layer_surface_unmap (self);
 
   wl_list_remove (&self->link);

src/layer_shell.c
@@ -203,7 +203,7 @@ arrange_layer (PhocOutput                     *output,
     // Apply
     struct wlr_box old_geo = layer_surface->geo;
     layer_surface->geo = box;
-    if (wlr_layer_surface->mapped) {
+    if (wlr_layer_surface->surface->mapped) {
       apply_exclusive (usable_area, state->anchor, state->exclusive_zone,
                        state->margin.top, state->margin.right,
                        state->margin.bottom, state->margin.left);
@@ -327,7 +327,7 @@ phoc_layer_shell_update_focus (void)
           continue;
 
         if (layer_surface->layer_surface->current.keyboard_interactive &&
-            layer_surface->layer_surface->mapped) {
+            layer_surface->layer_surface->surface->mapped) {
           topmost = layer_surface;
           break;
         }
@@ -344,7 +344,7 @@ phoc_layer_shell_update_focus (void)
           continue;
 
         if (layer_surface->layer_surface->current.keyboard_interactive &&
-            layer_surface->layer_surface->mapped) {
+            layer_surface->layer_surface->surface->mapped) {
           topmost = layer_surface;
           break;
         }
@@ -589,9 +589,9 @@ popup_create (struct wlr_xdg_popup *wlr_popup)
   }
   popup->wlr_popup = wlr_popup;
   popup->map.notify = popup_handle_map;
-  wl_signal_add(&wlr_popup->base->events.map, &popup->map);
+  wl_signal_add(&wlr_popup->base->surface->events.map, &popup->map);
   popup->unmap.notify = popup_handle_unmap;
-  wl_signal_add(&wlr_popup->base->events.unmap, &popup->unmap);
+  wl_signal_add(&wlr_popup->base->surface->events.unmap, &popup->unmap);
   popup->destroy.notify = popup_handle_destroy;
   wl_signal_add(&wlr_popup->base->events.destroy, &popup->destroy);
   popup->commit.notify = popup_handle_commit;
@@ -728,9 +728,9 @@ layer_subsurface_create(struct wlr_subsurface *wlr_subsurface)
   subsurface->wlr_subsurface = wlr_subsurface;
 
   subsurface->map.notify = subsurface_handle_map;
-  wl_signal_add(&wlr_subsurface->events.map, &subsurface->map);
+  wl_signal_add(&wlr_subsurface->surface->events.map, &subsurface->map);
   subsurface->unmap.notify = subsurface_handle_unmap;
-  wl_signal_add(&wlr_subsurface->events.unmap, &subsurface->unmap);
+  wl_signal_add(&wlr_subsurface->surface->events.unmap, &subsurface->unmap);
   subsurface->destroy.notify = subsurface_handle_destroy;
   wl_signal_add(&wlr_subsurface->events.destroy, &subsurface->destroy);
   subsurface->commit.notify = subsurface_handle_commit;
@@ -864,9 +864,9 @@ handle_layer_shell_surface (struct wl_listener *listener, void *data)
   layer_surface->destroy.notify = handle_destroy;
   wl_signal_add(&wlr_layer_surface->events.destroy, &layer_surface->destroy);
   layer_surface->map.notify = handle_map;
-  wl_signal_add(&wlr_layer_surface->events.map, &layer_surface->map);
+  wl_signal_add(&wlr_layer_surface->surface->events.map, &layer_surface->map);
   layer_surface->unmap.notify = handle_unmap;
-  wl_signal_add(&wlr_layer_surface->events.unmap, &layer_surface->unmap);
+  wl_signal_add(&wlr_layer_surface->surface->events.unmap, &layer_surface->unmap);
   layer_surface->new_popup.notify = handle_new_popup;
   wl_signal_add(&wlr_layer_surface->events.new_popup, &layer_surface->new_popup);
 

src/output.c
@@ -870,7 +870,7 @@ phoc_output_xwayland_children_for_each_surface (PhocOutput                  *sel
   struct wlr_xwayland_surface *child;
 
   wl_list_for_each (child, &surface->children, parent_link) {
-    if (child->mapped) {
+    if (child->surface->mapped) {
       double ox = child->x - output_box.x;
       double oy = child->y - output_box.y;
       phoc_output_surface_for_each_surface (self, child->surface, ox, oy, iterator,
@@ -1018,7 +1018,7 @@ phoc_output_drag_icons_for_each_surface (PhocOutput          *self,
 
     g_assert (PHOC_IS_SEAT (seat));
     PhocDragIcon *drag_icon = seat->drag_icon;
-    if (!drag_icon || !drag_icon->wlr_drag_icon->mapped)
+    if (!drag_icon || !drag_icon->wlr_drag_icon->surface->mapped)
       continue;
 
     double ox = drag_icon->x - output_box.x;

src/render.c
@@ -441,7 +441,7 @@ scan_out_fullscreen_view (PhocOutput *output)
 
     g_assert (PHOC_IS_SEAT (seat));
     PhocDragIcon *drag_icon = seat->drag_icon;
-    if (drag_icon && drag_icon->wlr_drag_icon->mapped) {
+    if (drag_icon && drag_icon->wlr_drag_icon->surface->mapped) {
       return false;
     }
   }

src/seat.c
@@ -762,9 +762,9 @@ phoc_seat_handle_start_drag (struct wl_listener *listener,
   icon->surface_commit.notify = phoc_drag_icon_handle_surface_commit;
   wl_signal_add (&wlr_drag_icon->surface->events.commit, &icon->surface_commit);
   icon->unmap.notify = phoc_drag_icon_handle_unmap;
-  wl_signal_add (&wlr_drag_icon->events.unmap, &icon->unmap);
+  wl_signal_add (&wlr_drag_icon->surface->events.unmap, &icon->unmap);
   icon->map.notify = phoc_drag_icon_handle_map;
-  wl_signal_add (&wlr_drag_icon->events.map, &icon->map);
+  wl_signal_add (&wlr_drag_icon->surface->events.map, &icon->map);
   icon->destroy.notify = phoc_drag_icon_handle_destroy;
   wl_signal_add (&wlr_drag_icon->events.destroy, &icon->destroy);
 

src/view.c
@@ -948,7 +948,7 @@ static void
 subsurface_handle_map (struct wl_listener *listener, void *data)
 {
   PhocServer *server = phoc_server_get_default ();
-  PhocSubsurface *subsurface = wl_container_of(listener, subsurface, map);
+  PhocSubsurface *subsurface = wl_container_of (listener, subsurface, map);
   PhocView *view = subsurface->child.view;
 
   subsurface->child.mapped = true;
@@ -971,7 +971,7 @@ static void
 subsurface_handle_unmap (struct wl_listener *listener,void *data)
 {
   PhocServer *server = phoc_server_get_default ();
-  PhocSubsurface *subsurface = wl_container_of(listener, subsurface, unmap);
+  PhocSubsurface *subsurface = wl_container_of (listener, subsurface, unmap);
 
   phoc_view_child_damage_whole (&subsurface->child);
   phoc_input_update_cursor_focus(server->input);
@@ -987,16 +987,16 @@ phoc_view_subsurface_create (PhocView *view, struct wlr_subsurface *wlr_subsurfa
   subsurface->wlr_subsurface = wlr_subsurface;
   phoc_view_child_init (&subsurface->child, &subsurface_impl,
                         view, wlr_subsurface->surface);
-  subsurface->child.mapped = wlr_subsurface->mapped;
+  subsurface->child.mapped = wlr_subsurface->surface->mapped;
 
   subsurface->destroy.notify = subsurface_handle_destroy;
   wl_signal_add (&wlr_subsurface->events.destroy, &subsurface->destroy);
 
   subsurface->map.notify = subsurface_handle_map;
-  wl_signal_add (&wlr_subsurface->events.map, &subsurface->map);
+  wl_signal_add (&wlr_subsurface->surface->events.map, &subsurface->map);
 
   subsurface->unmap.notify = subsurface_handle_unmap;
-  wl_signal_add (&wlr_subsurface->events.unmap, &subsurface->unmap);
+  wl_signal_add (&wlr_subsurface->surface->events.unmap, &subsurface->unmap);
 }
 
 static void
@@ -1009,16 +1009,16 @@ phoc_view_child_subsurface_create (PhocViewChild *child, struct wlr_subsurface *
   subsurface->wlr_subsurface = wlr_subsurface;
   phoc_view_child_init (&subsurface->child, &subsurface_impl, child->view,
                         wlr_subsurface->surface);
-  subsurface->child.mapped = wlr_subsurface->mapped;
+  subsurface->child.mapped = wlr_subsurface->surface->mapped;
 
   subsurface->destroy.notify = subsurface_handle_destroy;
   wl_signal_add (&wlr_subsurface->events.destroy, &subsurface->destroy);
 
   subsurface->map.notify = subsurface_handle_map;
-  wl_signal_add (&wlr_subsurface->events.map, &subsurface->map);
+  wl_signal_add (&wlr_subsurface->surface->events.map, &subsurface->map);
 
   subsurface->unmap.notify = subsurface_handle_unmap;
-  wl_signal_add (&wlr_subsurface->events.unmap, &subsurface->unmap);
+  wl_signal_add (&wlr_subsurface->surface->events.unmap, &subsurface->unmap);
 
   phoc_view_child_damage_whole (&subsurface->child);
 }

src/xdg-surface.c
@@ -310,7 +310,7 @@ handle_surface_commit (struct wl_listener *listener, void *data)
   PhocView *view = PHOC_VIEW (self);
   struct wlr_xdg_surface *surface = self->xdg_surface;
 
-  if (!surface->mapped)
+  if (!surface->surface->mapped)
     return;
 
   phoc_view_apply_damage (view);
@@ -554,10 +554,10 @@ phoc_xdg_surface_constructed (GObject *object)
   wl_signal_add (&self->xdg_surface->events.destroy, &self->destroy);
 
   self->map.notify = handle_map;
-  wl_signal_add (&self->xdg_surface->events.map, &self->map);
+  wl_signal_add (&self->xdg_surface->surface->events.map, &self->map);
 
   self->unmap.notify = handle_unmap;
-  wl_signal_add (&self->xdg_surface->events.unmap, &self->unmap);
+  wl_signal_add (&self->xdg_surface->surface->events.unmap, &self->unmap);
 
   self->request_move.notify = handle_request_move;
   wl_signal_add (&self->xdg_surface->toplevel->events.request_move, &self->request_move);

commit ed561d8b6af4bc114d71d6c260b60d0e79d23685

xwayland-surface: Setup view before maximizing
Otherwise initially maximized XWayland views aren't rendered.

Fixes: 1e9df0a2 ("treewide: Adjust to map logic changes")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/540>

src/xwayland-surface.c
@@ -455,20 +455,21 @@ handle_map (struct wl_listener *listener, void *data)
   self->surface_commit.notify = handle_surface_commit;
   wl_signal_add (&surface->surface->events.commit, &self->surface_commit);
 
-  if (surface->maximized_horz && surface->maximized_vert)
-    phoc_view_maximize (view, NULL);
-
-  phoc_view_auto_maximize (view);
-
   phoc_view_map (view, surface->surface);
 
-  if (!surface->override_redirect) {
+  if (surface->override_redirect)
+    phoc_view_set_initial_focus (view);
+  else {
     if (surface->decorations == WLR_XWAYLAND_SURFACE_DECORATIONS_ALL)
       phoc_view_set_decorated (view, TRUE);
+
     phoc_view_setup (view);
-  } else {
-    phoc_view_set_initial_focus (view);
   }
+
+  phoc_view_auto_maximize (PHOC_VIEW (self));
+
+  if (surface->maximized_horz && surface->maximized_vert)
+    phoc_view_maximize (view, NULL);
 }
 
 static void