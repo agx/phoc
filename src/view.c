agx/phoc

commit c2e903e61d7e4fce1d1f68d3b0ad2293a8007d63

view: Move subsurface initialization to separate function
That will make it easier to keep it in sync the same code for
childs.

src/view.c
@@ -704,6 +704,18 @@ static void view_child_handle_new_subsurface(struct wl_listener *listener,
 	phoc_view_subsurface_create(child->view, wlr_subsurface);
 }
 
+static void
+phoc_view_init_subsurfaces (PhocView *view, struct wlr_surface *surface)
+{
+  struct wlr_subsurface *subsurface;
+
+  wl_list_for_each(subsurface, &view->wlr_surface->subsurfaces_below, parent_link)
+    phoc_view_subsurface_create (view, subsurface);
+
+  wl_list_for_each(subsurface, &view->wlr_surface->subsurfaces_above, parent_link)
+    phoc_view_subsurface_create (view, subsurface);
+}
+
 void
 phoc_view_child_init (struct roots_view_child *child,
                       const struct phoc_view_child_interface *impl,
@@ -891,6 +903,7 @@ phoc_view_map (PhocView *view, struct wlr_surface *surface)
     phoc_view_subsurface_create(view, subsurface);
   }
 
+  phoc_view_init_subsurfaces (view, surface);
   view->surface_new_subsurface.notify = phoc_view_handle_surface_new_subsurface;
   wl_signal_add(&view->wlr_surface->events.new_subsurface, &view->surface_new_subsurface);
 

commit cd5b131f1fbdcdf73acb550ca279b11a69f4a09d

view: Don't init subsurfaces twice
The code in phoc_view_init_subsurfaces() does as same things the code in
map() just above the invocation does.

Fixes: c2e903e6 ("view: Move subsurface initialization to separate function")

which added the function but didn't remove the code in _map().

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/548>

src/view.c
@@ -928,14 +928,7 @@ phoc_view_map (PhocView *self, struct wlr_surface *surface)
   g_assert (self->wlr_surface == NULL);
   self->wlr_surface = surface;
 
-  struct wlr_subsurface *subsurface;
-  wl_list_for_each(subsurface, &self->wlr_surface->current.subsurfaces_below, current.link)
-    phoc_subsurface_new (self, subsurface);
-
-  wl_list_for_each(subsurface, &self->wlr_surface->current.subsurfaces_above, current.link)
-    phoc_subsurface_new (self, subsurface);
-
-  phoc_view_init_subsurfaces (self, surface);
+  phoc_view_init_subsurfaces (self, self->wlr_surface);
   priv->surface_new_subsurface.notify = phoc_view_handle_surface_new_subsurface;
   wl_signal_add (&self->wlr_surface->events.new_subsurface, &priv->surface_new_subsurface);
 