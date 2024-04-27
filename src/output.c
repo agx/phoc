31ae5c29bcff958e6b20a759bef794a2466f25bd
Guido Günther <agx@sigxcpu.org>
2024-04-27 12:51:44 +0200

    output: Fix indent
    Gbp-Dch: Ignore
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/561>

--- src/output.c
+++ src/output.c
@@ -908,9 +908,9 @@ phoc_output_initable_init (GInitable    *initable,
     if (priv->cutouts) {
       g_message ("Adding cutouts overlay");
       priv->cutouts_texture = phoc_cutouts_overlay_get_cutouts_texture (priv->cutouts, self);
-      priv->render_cutouts_id =  g_signal_connect_swapped (renderer, "render-end",
-                                                           G_CALLBACK (render_cutouts),
-                                                           self);
+      priv->render_cutouts_id = g_signal_connect_swapped (renderer, "render-end",
+                                                          G_CALLBACK (render_cutouts),
+                                                          self);
     } else {
       g_warning ("Could not create cutout overlay");
     }
