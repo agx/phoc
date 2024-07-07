agx/phoc

commit 109b0afe91a0f3256ba6a9f381ee2ad28a67fca4

output: Cache layer surface ordering
Instead of recalculating on each access cache the ordering
and make bits that might change the ordering (such as committing
a layer surface) invalidate the cache.

This also works towards removing more double accounting in this area.

TODO: Avoid direct access to the list of layer surfaces in
handle_layer_shell_surface and shell reveal

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/537>

src/layer_shell.c
@@ -372,6 +372,7 @@ handle_surface_commit (struct wl_listener *listener, void *data)
   PhocLayerSurface *layer_surface = wl_container_of (listener, layer_surface, surface_commit);
   struct wlr_layer_surface_v1 *wlr_layer_surface = layer_surface->layer_surface;
   struct wlr_output *wlr_output = wlr_layer_surface->output;
+
   if (wlr_output != NULL) {
     PhocOutput *output = PHOC_OUTPUT (wlr_output->data);
     struct wlr_box old_geo = layer_surface->geo;
@@ -379,6 +380,8 @@ handle_surface_commit (struct wl_listener *listener, void *data)
     bool layer_changed = false;
     if (wlr_layer_surface->current.committed != 0) {
       layer_changed = layer_surface->layer != wlr_layer_surface->current.layer;
+
+      phoc_output_set_layer_dirty (output, layer_surface->layer);
 
       layer_surface->layer = wlr_layer_surface->current.layer;
       phoc_layer_shell_arrange (output);
@@ -415,6 +418,8 @@ handle_surface_commit (struct wl_listener *listener, void *data)
                                        layer_surface->geo.x,
                                        layer_surface->geo.y);
     }
+
+    phoc_output_set_layer_dirty (output, layer_surface->layer);
   }
 }
 
@@ -423,6 +428,7 @@ static void
 handle_destroy (struct wl_listener *listener, void *data)
 {
   PhocLayerSurface *layer_surface = wl_container_of (listener, layer_surface, destroy);
+
   g_object_unref (layer_surface);
 }
 
@@ -907,7 +913,7 @@ phoc_handle_layer_shell_surface (struct wl_listener *listener, void *data)
   layer_surface->new_popup.notify = handle_new_popup;
   wl_signal_add (&wlr_layer_surface->events.new_popup, &layer_surface->new_popup);
 
-  PhocOutput *output = wlr_layer_surface->output->data;
+  PhocOutput *output = PHOC_OUTPUT (wlr_layer_surface->output->data);
   wl_list_insert (&output->layer_surfaces, &layer_surface->link);
 
   // Temporarily set the layer's current state to pending

src/layer-surface.c
@@ -125,6 +125,9 @@ phoc_layer_surface_finalize (GObject *object)
     phoc_layer_surface_unmap (self);
 
   wl_list_remove (&self->link);
+  if (output)
+    phoc_output_set_layer_dirty (output, self->layer);
+
   wl_list_remove (&self->destroy.link);
   wl_list_remove (&self->map.link);
   wl_list_remove (&self->unmap.link);

src/output.c
@@ -71,7 +71,7 @@ typedef struct _PhocOutputPrivate {
   PhocOutputScaleFilter  scale_filter;
   gboolean               gamma_lut_changed;
 
-  GQueue                *layer_surfaces;
+  GQueue                *layer_surfaces[ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY + 1];
 } PhocOutputPrivate;
 
 static void phoc_output_initable_iface_init (GInitableIface *iface);
@@ -953,7 +953,8 @@ phoc_output_finalize (GObject *object)
                  (GDestroyNotify)phoc_output_frame_callback_info_free);
 
   wl_list_init (&self->layer_surfaces);
-  g_clear_pointer (&priv->layer_surfaces, g_queue_free);
+  for (int i = 0; i < G_N_ELEMENTS (priv->layer_surfaces); i++)
+    g_clear_pointer (&priv->layer_surfaces[i], g_queue_free);
 
   g_clear_signal_handler (&priv->render_cutouts_id, priv->renderer);
   g_clear_object (&priv->renderer);
@@ -1246,15 +1247,18 @@ phoc_output_get_layer_surfaces_for_layer (PhocOutput *self, enum zwlr_layer_shel
   g_assert (PHOC_IS_OUTPUT (self));
   priv = phoc_output_get_instance_private (self);
 
-  g_clear_pointer (&priv->layer_surfaces, g_queue_free);
-  priv->layer_surfaces = g_queue_new ();
+  if (priv->layer_surfaces[layer])
+    return priv->layer_surfaces[layer];
+
+  g_clear_pointer (&priv->layer_surfaces[layer], g_queue_free);
+  priv->layer_surfaces[layer] = g_queue_new ();
 
   wl_list_for_each_reverse (layer_surface, &self->layer_surfaces, link) {
     if (layer_surface->layer != layer)
       continue;
 
     if (layer_surface->layer_surface->current.exclusive_zone > 0)
-      g_queue_push_head (priv->layer_surfaces, layer_surface);
+      g_queue_push_head (priv->layer_surfaces[layer], layer_surface);
   }
 
   wl_list_for_each (layer_surface, &self->layer_surfaces, link) {
@@ -1262,10 +1266,33 @@ phoc_output_get_layer_surfaces_for_layer (PhocOutput *self, enum zwlr_layer_shel
       continue;
 
     if (layer_surface->layer_surface->current.exclusive_zone <= 0)
-      g_queue_push_head (priv->layer_surfaces, layer_surface);
+      g_queue_push_head (priv->layer_surfaces[layer], layer_surface);
   }
 
-  return priv->layer_surfaces;
+  return priv->layer_surfaces[layer];
+}
+
+/**
+ * phoc_output_set_layer_dirty:
+ * @self: the output
+ * @layer: The layer to marks as dirty
+ *
+ * Invalidate the ordering of layer surfaces.
+ *
+ * Moving a layer surface between layers or changing the exclusive zone
+ * might affect a layer surfaces position in the stack. Calling this function
+ * invalidates the current ordering and makes sure the ordering is recalculated
+ * on next access.
+ */
+void
+phoc_output_set_layer_dirty (PhocOutput *self, enum zwlr_layer_shell_v1_layer layer)
+{
+  PhocOutputPrivate *priv;
+
+  g_assert (PHOC_IS_OUTPUT (self));
+  priv = phoc_output_get_instance_private (self);
+
+  g_clear_pointer (&priv->layer_surfaces[layer], g_queue_free);
 }
 
 /**

src/output.h
@@ -106,6 +106,7 @@ void        phoc_output_xwayland_children_for_each_surface (PhocOutput *self,
 #endif
 GQueue     *phoc_output_get_layer_surfaces_for_layer (PhocOutput                     *self,
                                                       enum zwlr_layer_shell_v1_layer  layer);
+void        phoc_output_set_layer_dirty (PhocOutput *self, enum zwlr_layer_shell_v1_layer  layer);
 
 /* signal handlers */
 void        phoc_handle_output_manager_apply (struct wl_listener *listener, void *data);

commit 8d0f637c8919410cfa21a43a7c65b507cf930797

layer-shell: Mark layer cache dirty when OSK changes layers
Fixes: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1075857
Fixes: 109b0afe9 ("output: Cache layer surface ordering")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Tested-by: Andrey Skvortsov <andrej.skvortzov@gmail.com>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/574>

src/layer_shell.c
@@ -946,6 +946,7 @@ phoc_layer_shell_update_osk (PhocOutput *output, gboolean arrange)
   PhocLayerSurface *osk;
   GSList *seats = phoc_input_get_seats (input);
   gboolean force_overlay = FALSE;
+  enum zwlr_layer_shell_v1_layer old_layer;
 
   g_assert (PHOC_IS_OUTPUT (output));
 
@@ -967,11 +968,17 @@ phoc_layer_shell_update_osk (PhocOutput *output, gboolean arrange)
     }
   }
 
+  old_layer = osk->layer;
   if (force_overlay && osk->layer != ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY)
     osk->layer = ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY;
 
   if (!force_overlay && osk->layer != osk->layer_surface->pending.layer)
     osk->layer = osk->layer_surface->pending.layer;
+
+  if (old_layer != osk->layer) {
+    phoc_output_set_layer_dirty (output, old_layer);
+    phoc_output_set_layer_dirty (output, osk->layer);
+  }
 
   if (force_overlay && arrange)
     phoc_layer_shell_arrange (output);