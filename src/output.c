agx/phoc

commit 128483f6a489348d76206bbe8fb2911a9b245561

output: Allow to configure physical width and height
This can be useful in case edid is off or if we're on a nested
display (like Wayland or X11)

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/473>

src/output.c
@@ -782,6 +782,12 @@ phoc_output_fill_state (PhocOutput              *self,
       g_warning ("Can only add modes for DRM backend");
     }
 
+    if (output_config->phys_width)
+      self->wlr_output->phys_width = output_config->phys_width;
+
+    if (output_config->phys_width)
+      self->wlr_output->phys_height = output_config->phys_height;
+
     if (output_config->mode.width)
       phoc_output_state_set_mode (self, pending, output_config);
     else if (preferred_mode != NULL)

src/settings.c
@@ -206,9 +206,9 @@ config_ini_handler (PhocConfig *config, const char *section, const char *name, c
         oc->mode.refresh_rate = strtof (end, &end);
         g_assert (strcmp ("Hz", end) == 0);
       }
-      g_debug ("Configured output %s with mode %dx%d@%f",
-               oc->name, oc->mode.width, oc->mode.height,
-               oc->mode.refresh_rate);
+      g_debug ("Parsed mode %dx%d@%f for output %s",
+               oc->mode.width, oc->mode.height,
+               oc->mode.refresh_rate, oc->name);
     } else if (strcmp (name, "modeline") == 0) {
       g_autofree PhocOutputModeConfig *mode = g_new0 (PhocOutputModeConfig, 1);
 
@@ -220,6 +220,12 @@ config_ini_handler (PhocConfig *config, const char *section, const char *name, c
       oc->scale_filter = parse_scale_filter (value);
     } else if (strcmp (name, "drm-panel-orientation") == 0) {
       oc->drm_panel_orientation = parse_boolean (value, true);
+    } else if (g_str_equal (name, "phys_width")) {
+      oc->phys_width = strtol (value, NULL, 10);
+    } else if (g_str_equal (name, "phys_height")) {
+      oc->phys_height = strtol (value, NULL, 10);
+    } else {
+      g_warning ("Unknown key '%s' in section '%s'", name, section);
     }
   } else {
     g_critical ("Found unknown config section: %s", section);

src/settings.h
@@ -29,6 +29,8 @@ typedef struct _PhocOutputConfig {
     float refresh_rate;
   } mode;
   GSList                  *modes;
+
+  guint                    phys_width, phys_height;
 } PhocOutputConfig;
 
 typedef struct _PhocConfig {

commit 13b3b3c7ccd734b9a24689d4ac1f61a46e34b9a7

output: Fix phys_height condition
We want to set it when the height is set, not the width.

Fixes: 128483f6a ("output: Allow to configure physical width and height")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/580>

src/output.c
@@ -785,7 +785,7 @@ phoc_output_fill_state (PhocOutput              *self,
     if (output_config->phys_width)
       self->wlr_output->phys_width = output_config->phys_width;
 
-    if (output_config->phys_width)
+    if (output_config->phys_height)
       self->wlr_output->phys_height = output_config->phys_height;
 
     if (output_config->mode.width)