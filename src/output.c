249e6839532f2e3d903fc2b1b2d69789e734ee02
Guido Günther <agx@sigxcpu.org>
2024-09-17 16:10:31 +0200

    output: Improve stacked critical
    It's way more useful if we know which surfaces cause the problem
    
    Gbp-Dch: Ignore
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/585>

--- src/output.c
+++ src/output.c
@@ -1309,7 +1309,9 @@ phoc_output_get_layer_surfaces_for_layer (PhocOutput *self, enum zwlr_layer_shel
       continue;
 
     if (phoc_layer_surface_get_layer (target) != phoc_layer_surface_get_layer (stacked)) {
-      g_critical ("Stacked surface and target surface not in same layer");
+      g_critical ("Stacked surface %s and target %s surface not in same layer",
+                  phoc_layer_surface_get_namespace (stacked),
+                  phoc_layer_surface_get_namespace (target));
       continue;
     }
 
