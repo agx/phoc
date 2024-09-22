agx/phoc

commit 11ac332cfcc665a5f16dc0d051a3047b13295419

cursor: Submit preedit on mouse/touch down
This ensures we submit the preedit when e.g.the user opens a menu
or clicks in another location in the text entry field

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/583>

src/cursor.c
@@ -27,6 +27,7 @@
 #include <linux/input-event-codes.h>
 #include "cursor.h"
 #include "desktop.h"
+#include "input-method-relay.h"
 #include "utils.h"
 #include "view.h"
 #include "xcursor.h"
@@ -1218,6 +1219,8 @@ phoc_cursor_press_button (PhocCursor              *self,
 
   if (!phoc_handle_shell_reveal (surface, lx, ly, PHOC_SHELL_REVEAL_POINTER_THRESHOLD) && !is_touch)
     send_pointer_button (seat, surface, time, button, state);
+
+  phoc_input_method_relay_im_submit (&seat->im_relay, surface);
 }
 
 
@@ -1411,6 +1414,8 @@ phoc_cursor_handle_touch_down (PhocCursor                  *self,
         phoc_seat_set_focus_layer (seat, wlr_layer);
       }
     }
+
+    phoc_input_method_relay_im_submit (&seat->im_relay, surface);
   }
 
   if (G_UNLIKELY (phoc_server_check_debug_flags (server, PHOC_SERVER_DEBUG_FLAG_TOUCH_POINTS))) {

commit d7fc40608b2e30a7248dff81eb539ecfe2362e59

cursor: Don't submit pending im state without a surface
Avoids hitting the assert() when dragging a window with server side
decorations.

Fixes: 11ac332cf ("cursor: Submit preedit on mouse/touch down")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/587>

src/cursor.c
@@ -1220,7 +1220,8 @@ phoc_cursor_press_button (PhocCursor              *self,
   if (!phoc_handle_shell_reveal (surface, lx, ly, PHOC_SHELL_REVEAL_POINTER_THRESHOLD) && !is_touch)
     send_pointer_button (seat, surface, time, button, state);
 
-  phoc_input_method_relay_im_submit (&seat->im_relay, surface);
+  if (surface)
+    phoc_input_method_relay_im_submit (&seat->im_relay, surface);
 }
 
 