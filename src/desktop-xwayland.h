9a9d863c5e07695ee0fe9d19dc5fe745834b00ac
Guido Günther <agx@sigxcpu.org>
2024-12-28 14:21:26 +0100

    desktop: Move XWayland setup to separate file
    Grouping XWayland related things together would have prompted for some
    code move so let's move to a separate file while at that.
    
    This allows us to minimize the #ifdef PHOC_XWAYLAND in PhocDesktop.
    
    Pure code move, no other changes.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/606>

---
+++ src/desktop-xwayland.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2024 The Phosh Developers;
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "desktop.h"
+#include "xwayland-surface.h"
+
+G_BEGIN_DECLS
+
+void phoc_desktop_setup_xwayland   (PhocDesktop *self);
+
+G_END_DECLS
