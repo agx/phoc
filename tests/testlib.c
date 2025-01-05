57adc0087ea17b5eb1a456137ed2f808a4529f7d
Guido Günther <agx@sigxcpu.org>
2025-01-05 13:29:52 +0100

    testlib: Avoid automatic cleanup
    This trips up static-analyzer and in this case it's bearable to not use
    `g_autoptr`. Should this happen more often we'd rather filter out these
    kind of errors.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/612>

--- tests/testlib.c
+++ tests/testlib.c
@@ -566,13 +566,14 @@ phoc_test_client_capture_frame (PhocTestClientGlobals *globals,
     guint32 height = frame->buffer.height;
     guint32 stride = frame->buffer.stride;
     guint8 *src = frame->buffer.shm_data;
-    g_autofree guint8 *dst = g_malloc0 (height * stride);
+    guint8 *dst = g_malloc0 (height * stride);
 
     for (guint i = 0, j = height - 1; i < height; i++, j--)
       memcpy ((dst + (i * stride)), (src + (j * stride)), stride);
 
     memcpy (src, dst, height * stride);
     frame->flags &= ~ZWLR_SCREENCOPY_FRAME_V1_FLAGS_Y_INVERT;
+    g_free (dst);
   }
   /* There shouldn't be any other flags left */
   g_assert_false (frame->flags);
