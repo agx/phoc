76c50f152898cfeedf94fbb3be88a6c473c28942
Guido Günther <agx@sigxcpu.org>
2025-01-29 10:37:33 +0100

    subsurface: Setup subsurface on property assignment
    This allows us to drop `constructed` and make things similar
    to `PhocSurface`.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/623>

--- src/subsurface.c
+++ src/subsurface.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 The Phosh Developers
+ * Copyright (C) 2024-2025 The Phosh Developers
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
@@ -56,6 +56,30 @@ subsurface_get_pos (PhocViewChild *child, int *sx, int *sy)
 
 
 static void
+subsurface_handle_destroy (struct wl_listener *listener, void *data)
+{
+  PhocSubsurface *self = wl_container_of (listener, self, destroy);
+
+  g_object_unref (self);
+}
+
+
+static void
+set_wlr_subsurface (PhocSubsurface *self, struct wlr_subsurface *subsurface)
+{
+  g_assert (self->wlr_subsurface == NULL);
+
+  self->wlr_subsurface = subsurface;
+  g_debug ("New surface %p", self->wlr_subsurface);
+
+  PHOC_VIEW_CHILD (self)->mapped = self->wlr_subsurface->surface->mapped;
+
+  self->destroy.notify = subsurface_handle_destroy;
+  wl_signal_add (&self->wlr_subsurface->events.destroy, &self->destroy);
+}
+
+
+static void
 phoc_subsurface_set_property (GObject      *object,
                               guint         property_id,
                               const GValue *value,
@@ -65,7 +89,7 @@ phoc_subsurface_set_property (GObject      *object,
 
   switch (property_id) {
   case PROP_WLR_SUBSURFACE:
-    self->wlr_subsurface = g_value_get_pointer (value);
+    set_wlr_subsurface (self, g_value_get_pointer (value));
     break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
@@ -94,30 +118,6 @@ phoc_subsurface_get_property (GObject    *object,
 
 
 static void
-subsurface_handle_destroy (struct wl_listener *listener, void *data)
-{
-  PhocSubsurface *self = wl_container_of (listener, self, destroy);
-
-  g_object_unref (self);
-}
-
-
-
-static void
-phoc_subsurface_constructed (GObject *object)
-{
-  PhocSubsurface *self = PHOC_SUBSURFACE (object);
-
-  G_OBJECT_CLASS (phoc_subsurface_parent_class)->constructed (object);
-
-  PHOC_VIEW_CHILD (self)->mapped = self->wlr_subsurface->surface->mapped;
-
-  self->destroy.notify = subsurface_handle_destroy;
-  wl_signal_add (&self->wlr_subsurface->events.destroy, &self->destroy);
-}
-
-
-static void
 phoc_subsurface_finalize (GObject *object)
 {
   PhocSubsurface *self = PHOC_SUBSURFACE (object);
@@ -134,7 +134,6 @@ phoc_subsurface_class_init (PhocSubsurfaceClass *klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
   PhocViewChildClass *view_child_class = PHOC_VIEW_CHILD_CLASS (klass);
 
-  object_class->constructed = phoc_subsurface_constructed;
   object_class->finalize = phoc_subsurface_finalize;
   object_class->get_property = phoc_subsurface_get_property;
   object_class->set_property = phoc_subsurface_set_property;
