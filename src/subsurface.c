2ea91a0bd5d57c0817c4de02a9ad2a50091ab2d3
Guido Günther <agx@sigxcpu.org>
2025-01-29 10:37:34 +0100

    subsurface: Track "external" damage due to moves and stacking changes
    Track damage triggered by subsurfaces changing their position in the list
    of above/below subsurfaces and due to moves relative to the parent
    surface.
    
    Fixes parts of wlroot's 7dfbd877 ("compositor: drop wlr_surface.external_damage")
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/623>

--- src/subsurface.c
+++ src/subsurface.c
@@ -11,6 +11,7 @@
 #include "phoc-config.h"
 
 #include "subsurface.h"
+#include "surface.h"
 
 
 enum {
@@ -29,12 +30,19 @@ typedef struct _PhocSubsurface {
   PhocViewChild          parent_instance;
 
   struct wlr_subsurface *wlr_subsurface;
+  struct previous {
+    gint32 x;
+    gint32 y;
+    struct wl_list *prev;
+    struct wl_list *next;
+  }                      previous;
 
+  struct wl_listener     parent_commit;
   struct wl_listener     destroy;
 } PhocSubsurface;
 
-
 G_DEFINE_FINAL_TYPE (PhocSubsurface, phoc_subsurface, PHOC_TYPE_VIEW_CHILD)
+
 
 static void
 subsurface_get_pos (PhocViewChild *child, int *sx, int *sy)
@@ -65,6 +73,62 @@ subsurface_handle_destroy (struct wl_listener *listener, void *data)
 
 
 static void
+collect_damage_iter (struct wlr_surface *wlr_surface, int sx, int sy, gpointer data)
+{
+  PhocSubsurface *self = PHOC_SUBSURFACE (data);
+  struct wlr_subsurface *wlr_subsurface = self->wlr_subsurface;
+  PhocSurface *surface = PHOC_SURFACE (wlr_subsurface->surface->data);
+  int current_x, current_y;
+
+  if (!surface)
+    return;
+
+  g_assert (PHOC_IS_SURFACE (surface));
+  phoc_view_child_get_pos (PHOC_VIEW_CHILD (self), &current_x, &current_y);
+
+  phoc_surface_add_damage_box (surface, &(struct wlr_box) {
+      /* We add the damage to the subsurface but `previous` is relative
+       * to the root surface, so subtract its offset */
+      self->previous.x - current_x,
+      self->previous.y - current_y,
+      wlr_surface->current.width,
+      wlr_surface->current.height
+    });
+}
+
+
+static void
+handle_parent_commit (struct wl_listener *listener, void *data)
+{
+  PhocSubsurface *self = wl_container_of (listener, self, parent_commit);
+  struct wlr_subsurface *wlr_subsurface = self->wlr_subsurface;
+  struct wlr_surface *wlr_surface = wlr_subsurface->surface;
+  gboolean moved, reordered;
+  int sx, sy;
+
+  phoc_view_child_get_pos (PHOC_VIEW_CHILD (self), &sx, &sy);
+
+  moved = (self->previous.x != sx || self->previous.y != sy);
+
+  reordered = (self->previous.prev != wlr_subsurface->current.link.prev ||
+               self->previous.next != wlr_subsurface->current.link.next);
+
+  if (wlr_subsurface->surface->mapped && (moved || reordered))
+    wlr_surface_for_each_surface (wlr_surface, collect_damage_iter, self);
+
+  self->previous.x = sx;
+  self->previous.y = sy;
+  self->previous.prev = wlr_subsurface->current.link.prev;
+  self->previous.next = wlr_subsurface->current.link.next;
+
+  if (wlr_subsurface->surface->mapped && (moved || reordered)) {
+    wlr_surface_for_each_surface (wlr_surface, collect_damage_iter, self);
+    phoc_view_child_apply_damage (PHOC_VIEW_CHILD (self));
+  }
+}
+
+
+static void
 set_wlr_subsurface (PhocSubsurface *self, struct wlr_subsurface *subsurface)
 {
   g_assert (self->wlr_subsurface == NULL);
@@ -76,6 +140,9 @@ set_wlr_subsurface (PhocSubsurface *self, struct wlr_subsurface *subsurface)
 
   self->destroy.notify = subsurface_handle_destroy;
   wl_signal_add (&self->wlr_subsurface->events.destroy, &self->destroy);
+
+  self->parent_commit.notify = handle_parent_commit;
+  wl_signal_add (&self->wlr_subsurface->parent->events.commit, &self->parent_commit);
 }
 
 
@@ -122,6 +189,7 @@ phoc_subsurface_finalize (GObject *object)
 {
   PhocSubsurface *self = PHOC_SUBSURFACE (object);
 
+  wl_list_remove (&self->parent_commit.link);
   wl_list_remove (&self->destroy.link);
 
   self->wlr_subsurface = NULL;
@@ -153,6 +221,8 @@ phoc_subsurface_class_init (PhocSubsurfaceClass *klass)
 static void
 phoc_subsurface_init (PhocSubsurface *self)
 {
+  self->previous.x = -1;
+  self->previous.y = -1;
 }
 
 
