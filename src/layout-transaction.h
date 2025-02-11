f394c402941d31fb44d2dba4079bb5585637e098
Guido Günther <agx@sigxcpu.org>
2025-02-11 10:35:07 +0100

    layout-transaction: Improve naming and debug messages
    We want to track configure for layer surfaces and views
    separately so add use proper naming.
    
    This triggers some reindent due to the rather long names now.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/643>

--- src/layout-transaction.h
+++ src/layout-transaction.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 The Phosh Developers
+ * Copyright (C) 2024-2025 The Phosh Developers
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
@@ -12,11 +12,13 @@ G_BEGIN_DECLS
 
 #define PHOC_TYPE_LAYOUT_TRANSACTION (phoc_layout_transaction_get_type ())
 
-G_DECLARE_FINAL_TYPE (PhocLayoutTransaction, phoc_layout_transaction, PHOC, LAYOUT_TRANSACTION, GObject)
+G_DECLARE_FINAL_TYPE (PhocLayoutTransaction, phoc_layout_transaction, PHOC, LAYOUT_TRANSACTION,
+                      GObject)
 
-PhocLayoutTransaction *phoc_layout_transaction_get_default (void);
-gboolean               phoc_layout_transaction_is_active (PhocLayoutTransaction *self);
-void                   phoc_layout_transaction_notify_configured (PhocLayoutTransaction *self);
-void                   phoc_layout_transaction_add_dirty (PhocLayoutTransaction *self);
+PhocLayoutTransaction *
+                  phoc_layout_transaction_get_default (void);
+gboolean          phoc_layout_transaction_is_active (PhocLayoutTransaction *self);
+void              phoc_layout_transaction_notify_layer_configured (PhocLayoutTransaction *self);
+void              phoc_layout_transaction_add_layer_dirty (PhocLayoutTransaction *self);
 
 G_END_DECLS
