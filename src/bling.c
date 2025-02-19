2d08abf5816a76bcaeddfa9e746a172f68135e11
Sam Day <me@samcday.com>
2025-02-19 12:33:12 +0100

    bling: Add phoc_bling_damage_box helper method
    Often, when a PhocBling needs to damage outputs, it simply needs to
    damage the whole region it occupies. This new phoc_bling_damage_box
    makes that trivial.
    
    Signed-off-by: Sam Day <me@samcday.com>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/646>

--- src/bling.c
+++ src/bling.c
@@ -8,6 +8,8 @@
 #define G_LOG_DOMAIN "phoc-bling"
 
 #include "bling.h"
+#include "desktop.h"
+#include "server.h"
 
 /**
  * PhocBling:
@@ -95,4 +97,37 @@ phoc_bling_is_mapped (PhocBling *self)
   g_assert (iface->is_mapped);
 
   return iface->is_mapped (self);
+}
+
+/**
+ * phoc_bling_damage_box:
+ * @self: the PhocBling that needs to damage output(s)
+ *
+ * A utility method to damage the entire bounding box region of a PhocBling.
+ */
+void
+phoc_bling_damage_box (PhocBling *self)
+{
+  PhocDesktop *desktop = phoc_server_get_desktop (phoc_server_get_default ());
+  PhocOutput *output;
+  struct wlr_box damage_box;
+
+  if (!phoc_bling_is_mapped (self))
+    return;
+
+  damage_box = phoc_bling_get_box (self);
+
+  wl_list_for_each (output, &desktop->outputs, link) {
+    bool intersects = wlr_output_layout_intersects (desktop->layout, output->wlr_output,
+                                                    &damage_box);
+    if (!intersects)
+      continue;
+
+    damage_box.x -= output->lx;
+    damage_box.y -= output->ly;
+    phoc_utils_scale_box (&damage_box, output->wlr_output->scale);
+
+    if (wlr_damage_ring_add_box (&output->damage_ring, &damage_box))
+      wlr_output_schedule_frame (output->wlr_output);
+  }
 }
